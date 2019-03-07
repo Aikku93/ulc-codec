d35f8d367d176ed09ab32ca4626eef030ecd0a04
Ruben <40356555+Aikku93@users.noreply.github.com>
2019-03-07 15:37:38 +1100

    simd-fied rotation butterflies

--- fourier/Fourier_DCT4.c
+++ fourier/Fourier_DCT4.c
@@ -10,7 +10,6 @@
 # include <xmmintrin.h>
 #endif
 /**************************************/
-#include <math.h>
 #include <stddef.h>
 /**************************************/
 #include "Fourier.h"
@@ -79,18 +78,106 @@ void Fourier_DCT4(float *Buf, float *Tmp, size_t N) {
 
 	//! Perform rotation butterflies
 	//!  u = R_n.x
-	//! TODO: SIMD opt
-	float c  = cos(M_PI_4 / N), s  = sin(M_PI_4 / N);
-	float wc = cos(M_PI_2 / N), ws = sin(M_PI_2 / N);
-	for(i=0;i<N/2;i++) {
-		float a = Buf[i];
-		float b = Buf[N-1-i];
-		Tmp[i]     = ( c*a + s*b);
-		Tmp[N-1-i] = (-s*a + c*b) * (i%2 ? (+1) : (-1));
-		float _c = wc*c - ws*s;
-		float _s = ws*c + wc*s;
-		c = _c;
-		s = _s;
+	{
+		float Ns = 1.0f / N;
+		const float *SrcLo = Buf;
+		const float *SrcHi = Buf + N;
+		      float *DstLo = Tmp;
+		      float *DstHi = Tmp + N;
+#if defined(__AVX__)
+		__m256 wc, ws; {
+			float c, s;
+			Fourier_SinCos(8.0f * Ns, &s, &c);
+			wc = _mm256_set1_ps(c);
+			ws = _mm256_set1_ps(s);
+		}
+		__m256 c, s; {
+			c = _mm256_setr_ps(0.5f, 1.5f, 2.5f, 3.5f, 4.5f, 5.5f, 6.5f, 7.5f);
+			c = _mm256_mul_ps(c, _mm256_set1_ps(Ns));
+			Fourier_SinCosAVX(c, &s, &c);
+		}
+
+		for(i=0;i<N/16;i++) {
+			SrcHi -= 8;
+			__m256 b = _mm256_load_ps(SrcHi);
+			__m256 a = _mm256_load_ps(SrcLo);
+			SrcLo += 8;
+			b = _mm256_shuffle_ps(b, b, 0x1B);
+			b = _mm256_permute2f128_ps(b, b, 0x01);
+
+			__m256 t1 = _mm256_sub_ps(_mm256_mul_ps(s, a), _mm256_mul_ps(c, b));
+			__m256 t0 = _mm256_add_ps(_mm256_mul_ps(c, a), _mm256_mul_ps(s, b));
+			t1 = _mm256_xor_ps(t1, _mm256_setr_ps(0.0f, -0.0f, 0.0f, -0.0f, 0.0f, -0.0f, 0.0f, -0.0f));
+			t1 = _mm256_shuffle_ps(t1, t1, 0x1B);
+			t1 = _mm256_permute2f128_ps(t1, t1, 0x01);
+
+			_mm256_store_ps(DstLo, t0); DstLo += 8;
+			DstHi -= 8; _mm256_store_ps(DstHi, t1);
+
+			t0 = _mm256_sub_ps(_mm256_mul_ps(wc, c), _mm256_mul_ps(ws, s));
+			t1 = _mm256_add_ps(_mm256_mul_ps(ws, c), _mm256_mul_ps(wc, s));
+			c = t0;
+			s = t1;
+		}
+#elif defined(__SSE__)
+		__m128 wc, ws; {
+			float c, s;
+			Fourier_SinCos(4.0f * Ns, &s, &c);
+			wc = _mm_set1_ps(c);
+			ws = _mm_set1_ps(s);
+		}
+		__m128 c, s; {
+			c = _mm_setr_ps(0.5f, 1.5f, 2.5f, 3.5f);
+			c = _mm_mul_ps(c, _mm_set1_ps(Ns));
+			Fourier_SinCosSSE(c, &s, &c);
+		}
+
+		for(i=0;i<N/8;i++) {
+			SrcHi -= 4;
+			__m128 b = _mm_loadr_ps(SrcHi);
+			__m128 a = _mm_load_ps(SrcLo);
+			SrcLo += 4;
+
+			__m128 t1 = _mm_sub_ps(_mm_mul_ps(s, a), _mm_mul_ps(c, b));
+			__m128 t0 = _mm_add_ps(_mm_mul_ps(c, a), _mm_mul_ps(s, b));
+			t1 = _mm_xor_ps(t1, _mm_setr_ps(0.0f, -0.0f, 0.0f, -0.0f));
+
+			_mm_store_ps(DstLo, t0); DstLo += 4;
+			DstHi -= 4; _mm_storer_ps(DstHi, t1);
+
+			t0 = _mm_sub_ps(_mm_mul_ps(wc, c), _mm_mul_ps(ws, s));
+			t1 = _mm_add_ps(_mm_mul_ps(ws, c), _mm_mul_ps(wc, s));
+			c = t0;
+			s = t1;
+		}
+#else
+#define UPDATE_SINCOS() \
+	do { \
+		_c = wc*c - ws*s; \
+		_s = ws*c + wc*s; \
+		c = _c; \
+		s = _s; \
+	} while(0)
+		float a, b;
+		float c, s, _c, _s;
+		float wc, ws;
+		Fourier_SinCos(0.5f * Ns, &s,  &c);
+		Fourier_SinCos(1.0f * Ns, &ws, &wc);
+		for(i=0;i<N/4;i++) {
+			a = *SrcLo++;
+			b = *--SrcHi;
+			*DstLo++ =  c*a + s*b;
+			*--DstHi =  s*a - c*b;
+			UPDATE_SINCOS();
+
+			a = *SrcLo++;
+			b = *--SrcHi;
+			*DstLo++ =  c*a + s*b;
+			*--DstHi = -s*a + c*b;
+			UPDATE_SINCOS();
+		}
+#undef UPDATE_SINCOS
+#endif
 	}
 
 	//! Perform recursion
