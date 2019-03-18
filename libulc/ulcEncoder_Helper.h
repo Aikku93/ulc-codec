b5c327ad1de05b2732bb0babc78ba894fe001737
Ruben <40356555+Aikku93@users.noreply.github.com>
2019-03-18 20:23:13 +1100

    Update ulcEncoder_Helper.h

--- libulc/ulcEncoder_Helper.h
+++ libulc/ulcEncoder_Helper.h
@@ -6,23 +6,12 @@
 #pragma once
 /**************************************/
 #include <stddef.h>
-#include <stdint.h>
 /**************************************/
 
 //! Integer floor(log2(x))
 //! Using unsigned int because that's what the compiler expects
 static inline size_t IntLog2(unsigned int x) {
 	return sizeof(unsigned int)*8-1 - __builtin_clz(x);
-}
-
-//! Get quantizer band from band index
-static size_t GetQuantBand(size_t Band, const uint16_t *QuantBw) {
-	size_t QBand;
-	for(QBand=0;;QBand++) {
-		size_t Bw = QuantBw[QBand];
-		if(Band < Bw) return QBand;
-		Band -= Bw;
-	}
 }
 
 /**************************************/
