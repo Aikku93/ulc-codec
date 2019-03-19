f7de09b464620409ee4b1ceaaa7884e7d3bc487c
Ruben <40356555+Aikku93@users.noreply.github.com>
2019-03-19 23:32:41 +1100

    unroll sample copy loop

--- libulc/ulcEncoder_BlockTransform.h
+++ libulc/ulcEncoder_BlockTransform.h
@@ -26,13 +26,31 @@ static void Block_Transform_CopySamples(float *DataDst, const float *DataSrc, si
 	size_t i;
 #if defined(__AVX__)
 	__m256 mScale = _mm256_set1_ps(Scale);
-	for(i=0;i<N;i+=16) {
-		__m256 v0 = _mm256_load_ps(DataSrc + i+0);
-		__m256 v1 = _mm256_load_ps(DataSrc + i+8);
+	for(i=0;i<N;i+=64) {
+		__m256 v0 = _mm256_load_ps(DataSrc + i+ 0);
+		__m256 v1 = _mm256_load_ps(DataSrc + i+ 8);
+		__m256 v2 = _mm256_load_ps(DataSrc + i+16);
+		__m256 v3 = _mm256_load_ps(DataSrc + i+24);
+		__m256 v4 = _mm256_load_ps(DataSrc + i+32);
+		__m256 v5 = _mm256_load_ps(DataSrc + i+40);
+		__m256 v6 = _mm256_load_ps(DataSrc + i+48);
+		__m256 v7 = _mm256_load_ps(DataSrc + i+56);
 		v0 = _mm256_mul_ps(v0, mScale);
 		v1 = _mm256_mul_ps(v1, mScale);
-		_mm256_store_ps(DataDst + i+0, v0);
-		_mm256_store_ps(DataDst + i+8, v1);
+		v2 = _mm256_mul_ps(v2, mScale);
+		v3 = _mm256_mul_ps(v3, mScale);
+		v4 = _mm256_mul_ps(v4, mScale);
+		v5 = _mm256_mul_ps(v5, mScale);
+		v6 = _mm256_mul_ps(v6, mScale);
+		v7 = _mm256_mul_ps(v7, mScale);
+		_mm256_store_ps(DataDst + i+ 0, v0);
+		_mm256_store_ps(DataDst + i+ 8, v1);
+		_mm256_store_ps(DataDst + i+16, v2);
+		_mm256_store_ps(DataDst + i+24, v3);
+		_mm256_store_ps(DataDst + i+32, v4);
+		_mm256_store_ps(DataDst + i+40, v5);
+		_mm256_store_ps(DataDst + i+48, v6);
+		_mm256_store_ps(DataDst + i+56, v7);
 	}
 #elif defined(__SSE__)
 	__m128 mScale = _mm_set1_ps(Scale);
@@ -41,14 +59,26 @@ static void Block_Transform_CopySamples(float *DataDst, const float *DataSrc, si
 		__m128 v1 = _mm_load_ps(DataSrc + i+ 4);
 		__m128 v2 = _mm_load_ps(DataSrc + i+ 8);
 		__m128 v3 = _mm_load_ps(DataSrc + i+12);
+		__m128 v4 = _mm_load_ps(DataSrc + i+16);
+		__m128 v5 = _mm_load_ps(DataSrc + i+20);
+		__m128 v6 = _mm_load_ps(DataSrc + i+24);
+		__m128 v7 = _mm_load_ps(DataSrc + i+27);
 		v0 = _mm_mul_ps(v0, mScale);
 		v1 = _mm_mul_ps(v1, mScale);
 		v2 = _mm_mul_ps(v2, mScale);
 		v3 = _mm_mul_ps(v3, mScale);
+		v4 = _mm_mul_ps(v4, mScale);
+		v5 = _mm_mul_ps(v5, mScale);
+		v6 = _mm_mul_ps(v6, mScale);
+		v7 = _mm_mul_ps(v7, mScale);
 		_mm_store_ps(DataDst + i+ 0, v0);
 		_mm_store_ps(DataDst + i+ 4, v1);
 		_mm_store_ps(DataDst + i+ 8, v2);
 		_mm_store_ps(DataDst + i+12, v3);
+		_mm_store_ps(DataDst + i+16, v4);
+		_mm_store_ps(DataDst + i+20, v5);
+		_mm_store_ps(DataDst + i+24, v6);
+		_mm_store_ps(DataDst + i+28, v7);
 	}
 #else
 	for(i=0;i<N;i++) DataDst[i] = DataSrc[i] * Scale;
