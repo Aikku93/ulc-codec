6511050c590c641e0b0e64d667d765b3b42df731
Ruben <40356555+Aikku93@users.noreply.github.com>
2019-04-04 23:21:40 +1100

    small cleanup, minor tuning

--- ulcplayer-gba/source/ulcplayer.s
+++ ulcplayer-gba/source/ulcplayer.s
@@ -11,17 +11,17 @@
 /**************************************/
 .equ GRAPH_W,  112 @ Pixels
 .equ GRAPH_H,   64 @ Pixels (NOTE: lower half reflected from top)
-.equ GRAPH_TAIL, 8
+.equ GRAPH_TAIL, 8 @ Pixels (NOTE: fixed 8px in code, just for reference)
 .equ GRAPHL_X,  56 @ Pixels
 .equ GRAPHL_Y,  48 @ Pixels
 .equ GRAPHR_X,  56 @ Pixels
 .equ GRAPHR_Y,  48 @ Pixels
-.equ ARTIST_X,  64 @ Pixels
+.equ ARTIST_X,  48 @ Pixels (NOTE: nominal, whole row is used)
 .equ ARTIST_Y,  16 @ Pixels
-.equ ARTIST_W, 112 @ Pixels
-.equ TITLE_X,   64 @ Pixels
+.equ ARTIST_W, 144 @ Pixels (NOTE: nominal, whole row is used)
+.equ TITLE_X,   48 @ Pixels (NOTE: nominal, whole row is used)
 .equ TITLE_Y,   32 @ Pixels
-.equ TITLE_W,  112 @ Pixels
+.equ TITLE_W,  144 @ Pixels (NOTE: nominal, whole row is used)
 .equ SPEAKER_LT_X,   8
 .equ SPEAKER_LT_Y,  24
 .equ SPEAKER_LB_X,   8
@@ -185,143 +185,129 @@ main:
 .arm
 UpdateGfx:
 	STMFD	sp!, {r4-fp,lr}
-	LDR	r0, =0x04000000
-	LDR	r1, =0x1F40
+	LDR	r4, =ulc_State
+0:	MOV	r0, #0x04000000
+	MOV	r1, #0x1F00
+	ORR	r1, r1, #0x40
 	STRH	r1, [r0]
-	LDR	r1, =GRAPHL_TILEMAP<<8 | (GRAPHR_TILEMAP<<8)<<16
+	MOV	r1, #GRAPHL_TILEMAP<<8
+	ORR	r1, r1, #(GRAPHR_TILEMAP<<8)<<16
 	STR	r1, [r0, #0x08]
-	LDR	r1, =GLYPHS_TILEMAP<<8 | (BGDESIGN_TILEMAP<<8)<<16
+	MOV	r1, #GLYPHS_TILEMAP<<8
+	ORR	r1, r1, #(BGDESIGN_TILEMAP<<8)<<16
 	STR	r1, [r0, #0x0C]
 	LDR	r1, =((-GRAPHL_X)&0xFFFF) | ((-GRAPHL_Y)&0xFFFF)<<16
 	STR	r1, [r0, #0x10]
 	LDR	r1, =((-GRAPHR_X)&0xFFFF) | ((-GRAPHR_Y)&0xFFFF)<<16
 	STR	r1, [r0, #0x14]
-	LDR	r1, =0
+	MOV	r1, #0x00
 	STR	r1, [r0, #0x18]
 	STR	r1, [r0, #0x1C]
 	LDR	r1, =0x10102F53
-	STR	r1, [r0, #0x50] @ Layer BG0,BG1,OBJ over BG0,BG1,BG2,BG3, additive blend
+	STR	r1, [r0, #0x50]!       @ Layer BG0,BG1,OBJ over BG0,BG1,BG2,BG3, additive blend
+0:	LDRH	r5, [r0, #0x0104-0x50] @ Get SmpPos from timer -> r5
+	LDRB	ip, [r4, #0x00]
+	SUB	r5, r5, #0x010000+BLOCK_SIZE @ Adjust for double buffer
+	ADD	r5, r5, ip, lsl #BLOCK_SIZE_LOG2
 
 .LRedraw_Clear:
-1:	LDR	r0, =GLYPHS_TILEADR + ((ARTIST_Y/8)*32 + ARTIST_X/8)*2
-	LDR	r1, =0
-	LDR	r2, =ARTIST_W*2
+1:	LDR	r0, =GLYPHS_TILEADR + ((ARTIST_Y/8)*32)*2
+	MOV	r1, #0x00
+	MOV	r2, #0x20*2 @ Clear whole row
 	BL	.LRedraw_Set32
-1:	LDR	r0, =GLYPHS_TILEADR + ((TITLE_Y/8)*32 + TITLE_X/8)*2
-	LDR	r1, =0
-	LDR	r2, =TITLE_W*2
+1:	LDR	r0, =GLYPHS_TILEADR + ((TITLE_Y/8)*32)*2
+	MOV	r1, #0x00
+	MOV	r2, #0x20*2 @ Clear whole row
 	BL	.LRedraw_Set32
 1:	LDR	r0, =GRAPHL_TILEADR
-	LDR	r1, =0
-	LDR	r2, =GRAPH_NTILES * 32 * 2 @ Clear L+R
+	MOV	r1, #0x00
+	MOV	r2, #GRAPH_NTILES * 32 * 2 @ Clear L+R
 	BL	.LRedraw_Set32
 
 .LRedraw_DrawTitle:
-1:	LDR	r0, =GLYPHS_TILEADR + ((ARTIST_Y/8)*32 + ARTIST_X/8)*2
+1:	LDR	r0, =GLYPHS_TILEADR + ((ARTIST_Y/8)*32)*2
 	LDR	r1, =SoundFile_Artist
-	LDR	r2, =ARTIST_W
 	BL	.LRedraw_DrawString
-1:	LDR	r0, =GLYPHS_TILEADR + ((TITLE_Y/8)*32 + TITLE_X/8)*2
+1:	LDR	r0, =GLYPHS_TILEADR + ((TITLE_Y/8)*32)*2
 	LDR	r1, =SoundFile_Title
-	LDR	r2, =TITLE_W
 	BL	.LRedraw_DrawString
 
 .LRedraw_GetSamples:
 	LDR	r0, =.LRedraw_GraphDataL
-	LDR	r1, =.LRedraw_GraphDataR
-	LDR	r2, =ulc_OutputBuffer + BLOCK_SIZE*2
-	LDR	r3, =ulc_OutputBuffer + BLOCK_SIZE*2 + BLOCK_SIZE*2
-	LDR	r4, =ulc_State
+	MOV	r1, #(BLOCK_SIZE*2) >> 8             @ Distance to right channel
+	SUB	r1, r1, #GRAPH_W<<24
+	LDR	r3, =ulc_OutputBuffer + BLOCK_SIZE*2 @ End -> r3
+	ADD	r2, r3, r5                           @ Src -> r2
 	LDR	r4, [r4, #0x08]
 	LDR	r4, [r4, #0x0C]
-	LDR	ip, =GRAPH_SMPSTRIDE_RCP
-	MUL	r4, ip, r4 @ Step[.22fxp]
-	LDR	r5, .LRedraw_PosMu
-	LDR	r6, .LRedraw_LowPassL
-	LDR	r7, .LRedraw_LowPassR
-	MOV	r8, #0x00 @ PowL = 0 (for speaker effect)
-	MOV	r9, #0x00 @ PowR = 0
-	LDR	sl, .LRedraw_BufOfs
-	LDR	fp, =GRAPH_W
-1:	LDRB	ip, [r2, sl]            @ Abs[xL] -> ip
-	CMP	ip, #0x80
-	RSBCS	ip, ip, #0x0100
-	RSB	lr, r6, ip, lsl #0x10   @ LP_L = LP_L + (xL - LP_L)*1/64
-	ADD	r6, r6, lr, asr #0x06   @ NOTE: LP_L in 8.16
-	ADD	r8, r8, r6
-	LDRB	lr, [r0]
+	LDR	r5, =GRAPH_SMPSTRIDE_RCP
+	MUL	r4, r5, r4 @ Step[.22fxp]
+	MOV	r5, #0x00  @ PosMu (not important to track accurately across frames)
+	MOV	r6, #0x00  @ PowL = 0 (for speaker effect)
+	MOV	r7, #0x00  @ PowR = 0
+	LDR	r8, .LRedraw_LowPassL
+	LDR	r9, .LRedraw_LowPassR
+1:	LDRB	ip, [r2, r1, lsl #0x08] @ Abs[xR] -> ip
+	LDRB	lr, [r0, #GRAPH_W]      @ OldR -> lr
+	TST	ip, #0x80
+	RSBNE	ip, ip, #0x0100
 	SUB	ip, ip, lr              @ Combine with old (nicer effect)
 	ADD	ip, lr, ip, asr #0x03
-	STRB	ip, [r0], #0x01
-	LDRB	ip, [r3, sl]            @ Abs[xR] -> ip
-	CMP	ip, #0x80
-	RSBCS	ip, ip, #0x0100
-	RSB	lr, r7, ip, lsl #0x10   @ LP_R = LP_R + (xR - LP_R)*1/64
-	ADD	r7, r7, lr, asr #0x06   @ NOTE: LP_R in 8.16
-	ADD	r9, r9, r7
-	LDRB	lr, [r1]
+	STRB	ip, [r0, #GRAPH_W]
+	RSB	lr, r9, ip, lsl #0x10   @ LP_R = LP_R + (xR - LP_R)*1/32
+	ADD	r9, r9, lr, asr #0x05   @ NOTE: LP_R in 8.16
+	ADD	r7, r7, r9              @ PowR += LP_R
+0:	ADD	r5, r5, r4              @  PosMu += Step
+	LDRB	ip, [r2], r5, lsr #0x16 @ Abs[xL] -> ip, update position
+	LDRB	lr, [r0]                @ OldL -> lr
+	BIC	r5, r5, #0xFF<<22       @  Clear integer part (step is less than 8 at sane rates, so clearing only up to 255 is more than fine)
+	CMP	r2, r3                  @  Wrap
+	SUBCS	r2, r2, #BLOCK_SIZE*2
+	TST	ip, #0x80
+	RSBNE	ip, ip, #0x0100
 	SUB	ip, ip, lr              @ Combine with old
 	ADD	ip, lr, ip, asr #0x03
-	STRB	ip, [r1], #0x01
-	ADD	r5, r5, r4            @ PosMu += Step
-	ADDS	sl, sl, r5, lsr #0x16 @ Update position, wrap, clear integer part of PosMu
-	SUBCS	sl, sl, #BLOCK_SIZE*2
-	BIC	r5, r5, #0xFF<<22     @ (step is less than 8 at sane rates, so clearing only up to 255 is more than fine)
-	SUBS	fp, fp, #0x01
-	BNE	1b
-2:	STR	sl, .LRedraw_BufOfs @ Updated in VBlank or can go out of sync from long decode times
-	STR	r5, .LRedraw_PosMu
-	STR	r6, .LRedraw_LowPassL
-	STR	r7, .LRedraw_LowPassR
+	STRB	ip, [r0], #0x01
+	RSB	lr, r8, ip, lsl #0x10   @ LP_L = LP_L + (xL - LP_L)*1/32
+	ADD	r8, r8, lr, asr #0x05   @ NOTE: LP_L in 8.16
+	ADD	r6, r6, r8              @ PowL += LP_L
+	ADDS	r1, r1, #0x01<<24
+	BCC	1b
+2:	STR	r8, .LRedraw_LowPassL
+	STR	r9, .LRedraw_LowPassR
 
 .LRedraw_DrawSpeakers:
-	LDR	r2, .LRedraw_PowerOldL
-	LDR	r3, .LRedraw_PowerOldR
-	SUBS	ip, r8, r2
-	ADDHI	r2, r2, ip, asr #0x01 @ Dampen attack (slight)
-	ADDCC	r2, r2, ip, asr #0x04 @ Dampen decay (heavy)
-	SUBS	ip, r9, r3
-	ADDHI	r3, r3, ip, asr #0x01
-	ADDCC	r3, r3, ip, asr #0x04
-	STR	r2, .LRedraw_PowerOldL
-	STR	r3, .LRedraw_PowerOldR
-	UMULL	r8, r9, r2, r2    @ Arbitrary scaling
-	UMULL	r2, ip, r3, r3
-	MOV	r2, r9, lsr #0x17
-	MOV	r3, ip, lsr #0x17
-	ADD	ip, r2, r3        @ Using input bits as source of entropy for speaker animation
-	AND	ip, ip, #0x03<<1
-	CMP	ip, #0x02<<1
-	MOVHI	ip, #0x02<<1
-	CMP	r2, #0x02
-	MOVHI	r2, #0x03
-	ADDCS	r2, r2, ip
-	CMP	r3, #0x02
-	MOVHI	r3, #0x03
-	ADDCS	r3, r3, ip
-	MOV	r2, r2, lsl #0x04 @ 16x 8x8 tiles per 32x32 area
-	MOV	r3, r3, lsl #0x04 @ 16x 8x8 tiles per 32x32 area
-	LDR	r5, =0x07000000
-	STRH	r2, [r5, #0x08*0+0x04] @ L-T (tile in Attr2 bit 0..9)
-	STRH	r2, [r5, #0x08*1+0x04] @ L-B
-	STRH	r3, [r5, #0x08*2+0x04] @ R-T
-	STRH	r3, [r5, #0x08*3+0x04] @ R-B
+	UMULL	ip, r2, r6, r6          @ Reduce low-power disturbance (x^4)
+	UMULL	ip, r6, r2, r2
+	UMULL	ip, r3, r7, r7
+	UMULL	ip, r7, r3, r3
+	MVN	ip, #0x0F
+	AND	r2, ip, r6, lsr #0x0F-4 @ 16x 8x8 tiles per 32x32 area (arbitrary scaling)
+	AND	r3, ip, r7, lsr #0x0F-4
+	CMP	r2, #0x07<<4            @ Clip animation frames
+	MOVHI	r2, #0x07<<4
+	CMP	r3, #0x07<<4
+	MOVHI	r3, #0x07<<4
+0:	LDR	ip, =0x07000000
+	STRH	r2, [ip, #0x08*0+0x04] @ L-T (tile in Attr2 bit 0..9)
+	STRH	r2, [ip, #0x08*1+0x04] @ L-B
+	STRH	r3, [ip, #0x08*2+0x04] @ R-T
+	STRH	r3, [ip, #0x08*3+0x04] @ R-B
 
 .LRedraw_DrawGraphs:
 	LDR	r2, =GRAPHL_TILEADR + (GRAPH_H/2-1)*4 + (GRAPH_H/2)*(GRAPH_TAIL/8)*4
-	LDR	r3, =GRAPHR_TILEADR + (GRAPH_H/2-1)*4 + (GRAPH_H/2)*(GRAPH_TAIL/8)*4
 1:	MOV	r5, #0x01            @ PixelStep
-	LDRB	r6, [r0, #-GRAPH_W]! @ L-side (L), rewind
+	LDRB	r6, [r0, #-GRAPH_W]! @ L-side (L)
 	ADR	lr, 0f
 0:	MOV	r5, r5, ror #0x04
 	SUB	r7, r2, #(GRAPH_H/2)*(GRAPH_TAIL/8)*4
 	MOVS	r6, r6, lsr #0x01
 	BNE	.LRedraw_DrawGraphBar
 2:	MOV	r5, #0x01
-	LDRB	r6, [r1, #-GRAPH_W]! @ L-side (R), rewind
+	LDRB	r6, [r0, #GRAPH_W]   @ L-side (R)
 	ADR	lr, 0f
 0:	MOV	r5, r5, ror #0x04
-	SUB	r7, r3, #(GRAPH_H/2)*(GRAPH_TAIL/8)*4
+	SUB	r7, r2, #(GRAPH_H/2)*(GRAPH_TAIL/8)*4 - (GRAPHR_TILEADR-GRAPHL_TILEADR)
 	MOVS	r6, r6, lsr #0x01
 	BNE	.LRedraw_DrawGraphBar
 1:	MOV	r5, #0x10000000
@@ -332,26 +318,25 @@ UpdateGfx:
 	MOVS	r6, r6, lsr #0x01
 	BNE	.LRedraw_DrawGraphBar
 2:	MOV	r5, #0x10000000
-	LDRB	r6, [r1, #GRAPH_W-1] @ R-side (R)
+	LDRB	r6, [r0, #GRAPH_W*2-1] @ R-side (R)
 	ADR	lr, 0f
 0:	MOV	r5, r5, ror #0x1C
-	ADD	r7, r3, #(GRAPH_H/2)*(GRAPH_W/8)*4
+	ADD	r7, r2, #(GRAPH_H/2)*(GRAPH_W/8)*4 + (GRAPHR_TILEADR-GRAPHL_TILEADR)
 	MOVS	r6, r6, lsr #0x01
 	BNE	.LRedraw_DrawGraphBar
-	LDR	r4, =GRAPH_W
+3:	LDR	r4, =GRAPH_W
 	MOV	r5, #0x01 @ PixelStep (will be rotated every pixel)
-1:	LDRB	r6, [r0], #0x01   @ Value -> r6
+1:	LDRB	r6, [r0, #GRAPH_W] @ ValueR -> r6
+	ADD	r7, r2, #GRAPHR_TILEADR-GRAPHL_TILEADR
+	MOVS	r6, r6, lsr #0x01  @ Rescale
+	BLNE	.LRedraw_DrawGraphBar
+2:	LDRB	r6, [r0], #0x01    @ ValueL -> r6
 	MOV	r7, r2
-	MOVS	r6, r6, lsr #0x01 @ Rescale
+	MOVS	r6, r6, lsr #0x01  @ Rescale
 	BLNE	.LRedraw_DrawGraphBar
-2:	LDRB	r6, [r1], #0x01   @ Value -> r6
-	MOV	r7, r3
-	MOVS	r6, r6, lsr #0x01 @ Rescale
-	BLNE	.LRedraw_DrawGraphBar
-3:	MOV	r5, r5, ror #0x1C @ Rotate PixelStep
-	CMP	r5, #0x01         @ Wrapped around? Move to next tile
+3:	MOV	r5, r5, ror #0x1C  @ Rotate PixelStep
+	CMP	r5, #0x01          @ Wrapped around? Move to next tile
 	ADDEQ	r2, r2, #(GRAPH_H/2)*4
-	ADDEQ	r3, r3, #(GRAPH_H/2)*4
 	SUBS	r4, r4, #0x01
 	BHI	1b
 
@@ -359,32 +344,36 @@ UpdateGfx:
 	LDMFD	sp!, {r4-fp,lr}
 	BX	lr
 
+@ NOTE: Copying 128 bits at a time
 .LRedraw_Set32:
-1:	STR	r1, [r0], #0x04
-	SUBS	r2, r2, #0x04
-	BHI	1b
-2:	BX	lr
+	STR	lr, [sp, #-0x04]!
+	MOV	r3, r1
+	MOV	ip, r1
+	MOV	lr, r1
+1:	STMIA	r0!, {r1,r3,ip,lr}
+	SUBS	r2, r2, #0x10
+	BNE	1b
+2:	LDR	pc, [sp], #0x04
 
 @ r0: &Dst
 @ r1: &String
-@ r2:  WidthPx
 .LRedraw_DrawString:
-	MOV	r3, r1
-1:	LDRB	ip, [r3], #0x01
-	CMP	ip, #0x01
+	MOV	r2, r1
+1:	LDRB	r3, [r2], #0x01
+	CMP	r3, #0x01
 	BCS	1b
-2:	SBC	r3, r3, r1
-	RSB	r3, r3, r2, lsr #0x03
-	MOV	r3, r3, lsr #0x01
-	ADD	r0, r0, r3, lsl #0x01
-	LDR	r3, =GLYPHS_TILEOFS + (GLYPHS_PAL16<<12)
-0:	LDRB	ip, [r1], #0x01
-1:	SUBS	ip, ip, #0x21
-	ADDCS	ip, ip, r3
-	MOVCC	ip, #0x00
-	STRH	ip, [r0], #0x02
-	LDRB	ip, [r1], #0x01
-	CMP	ip, #0x00
+2:	SBC	r2, r2, r1
+	RSB	r2, r2, #0x1E
+	MOV	r2, r2, lsr #0x01
+	ADD	r0, r0, r2, lsl #0x01
+	LDR	r2, =GLYPHS_TILEOFS + (GLYPHS_PAL16<<12)
+0:	LDRB	r3, [r1], #0x01
+1:	SUBS	r3, r3, #0x21
+	ADDCS	r3, r3, r2
+	MOVCC	r3, #0x00
+	STRH	r3, [r0], #0x02
+	LDRB	r3, [r1], #0x01
+	CMP	r3, #0x00
 	BNE	1b
 2:	BX	lr
 
@@ -413,13 +402,8 @@ UpdateGfx:
 	BHI	2b
 4:	BX	lr
 
-.LRedraw_BufOfs: .word -BLOCK_SIZE*2
-.LRedraw_PosMu:  .word 0
 .LRedraw_LowPassL: .word 0
 .LRedraw_LowPassR: .word 0
-.LRedraw_PowerOldL: .word 0
-.LRedraw_PowerOldR: .word 0
-
 .LRedraw_GraphDataL: .space GRAPH_W
 .LRedraw_GraphDataR: .space GRAPH_W
 
