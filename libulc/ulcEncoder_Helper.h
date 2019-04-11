4a540ee5fcd0dfbc1928751d47cf7c39d2435650
Ruben <40356555+Aikku93@users.noreply.github.com>
2019-04-11 18:14:58 +1000

    move ABS(),SQR() here, add spectral flatness
    Currently not using spectral flatness measure, but might come in handy at some point.

--- libulc/ulcEncoder_Helper.h
+++ libulc/ulcEncoder_Helper.h
@@ -7,11 +7,35 @@
 /**************************************/
 #include <stddef.h>
 /**************************************/
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#define SQR(x) ((x)*(x))
+/**************************************/
 
 //! Integer floor(log2(x))
 //! Using unsigned int because that's what the compiler expects
 static inline size_t IntLog2(unsigned int x) {
 	return sizeof(unsigned int)*8-1 - __builtin_clz(x);
+}
+
+//! Spectral flatness measure
+//! Adapted from "Note on measures for spectral flatness"
+//! DOI: 10.1049/el.2009.1977
+static inline double SpectralFlatness(const float *Buf, size_t N) {
+	size_t i;
+
+	//! Get normalization factor
+	double Nrm = 0.0;
+	for(i=0;i<N;i++) Nrm += SQR((double)Buf[i]);
+	if(Nrm == 0.0) return 1.0;
+	Nrm = 1.0 / Nrm;
+
+	//! Measure
+	double Sum = 0.0;
+	for(i=0;i<N;i++) {
+		double v = SQR((double)Buf[i]) * Nrm;
+		if(v != 0.0) Sum += v*log(v);
+	}
+	return pow(2.0, -Sum/log(N)) - 1.0;
 }
 
 /**************************************/
