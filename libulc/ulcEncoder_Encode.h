28f6918bdfa979446ea5ec944a434746ab0ce44e
Ruben <40356555+Aikku93@users.noreply.github.com>
2019-04-12 21:53:11 +1000

    update analysis key structure

--- libulc/ulcEncoder_Encode.h
+++ libulc/ulcEncoder_Encode.h
@@ -29,8 +29,8 @@ static inline void Block_Encode_WriteNybble(uint8_t x, uint8_t **Dst, size_t *Si
 	if((*Size)%8u == 0) (*Dst)++;
 }
 static size_t Block_Encode(const struct ULC_EncoderState_t *State, uint8_t *DstBuffer, size_t nNzMax, size_t nKeys, size_t *_nNzCoded) {
-	//! Generate quantizers and get number of non-zero bands
-	size_t nNzBands = Block_Encode_BuildQuants(State, nNzMax, nKeys);
+	//! Process the keys and determine how many we're working with
+	size_t nKeysEncode = Block_Encode_ProcessKeys(State, nNzMax, nKeys);
 
 	//! Spill state to local variables to make things easier to read
 	//! PONDER: Hopefully the compiler realizes that State is const and
@@ -40,15 +40,6 @@ static size_t Block_Encode(const struct ULC_EncoderState_t *State, uint8_t *DstB
 	float **TransformBuffer = State->TransformBuffer;
 	struct AnalysisKey_t *Keys = State->AnalysisKeys;
 
-	//! Sort keys by band index
-	//! This avoids a search for the next non-zero band
-	//! Also because the channel is coded in the high bits, we can
-	//! code one channel at a time, making things easier as well
-	size_t nChanLog2     = IntLog2(nChan);
-	size_t BlockSizeLog2 = IntLog2(BlockSize);
-	if(nChan > (1u<<nChanLog2)) nChanLog2++; //! Round to next power of two
-	Analysis_KeysSort(Keys, Keys+nNzBands, 1u << (nChanLog2 + BlockSizeLog2 - 1));
-
 	//! Start coding
 	size_t Chan;
 	size_t Key      = 0;
@@ -57,9 +48,9 @@ static size_t Block_Encode(const struct ULC_EncoderState_t *State, uint8_t *DstB
 	for(Chan=0;Chan<nChan;Chan++) {
 		//! Code the coefficients
 		//! NOTE:
-		//!  Check Key<nNzBands: If the last channel is silent, this avoids trying to code it by accident
+		//!  Check Key < nKeysEncode: If the last channel is silent, this avoids trying to code it by accident
 		//!  Check Key.Chan==Chan: This correctly codes silent channels
-		if(Key < nNzBands && (Keys[Key].Key >> BlockSizeLog2) == Chan) {
+		if(Key < nKeysEncode && Keys[Key].Chan == Chan) {
 			//! Code the first quantizer ([8h,0h,]0h..Eh) and start coding
 			size_t  NextBand  = 0;
 			int16_t LastQuant = Keys[Key].Quant;
@@ -67,9 +58,14 @@ static size_t Block_Encode(const struct ULC_EncoderState_t *State, uint8_t *DstB
 			do {
 				//! Unpack key data
 				//! If we cross to the next channel, break out
-				size_t tChan = Keys[Key].Key >> BlockSizeLog2; if(tChan != Chan) break;
-				size_t tBand = Keys[Key].Key & (BlockSize-1);
-
+#if 0
+				size_t tChan = Keys[Key].Chan; if(tChan != Chan) break;
+				size_t tBand = Keys[Key].Band;
+#else
+				size_t KeyVal = Keys[Key].Key;
+				size_t tChan  = KeyVal >> 16; if(tChan != Chan) break;
+				size_t tBand  = (uint16_t)KeyVal;
+#endif
 				//! Code the zero runs
 				//! NOTE: Escape-code-coded zero runs have a minimum size of 4 coefficients
 				//!       This is because two zero coefficients can be coded as 0h,0h, so
@@ -129,7 +125,7 @@ static size_t Block_Encode(const struct ULC_EncoderState_t *State, uint8_t *DstB
 					Block_Encode_WriteNybble(Qn, &DstBuffer, &Size);
 				} while(++NextBand <= tBand);
 				nNzCoded++;
-			} while(++Key < nNzBands);
+			} while(++Key < nKeysEncode);
 
 			//! 8h,0h,Fh: Stop
 			//! If we're at the edge of the block, it might work better to just fill with 0h
