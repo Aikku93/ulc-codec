abf1b816578321d05c8eb1ed10807f3104ac0e9f
Aikku93 <aik@aol.com.au>
2020-03-13 18:55:37 +1100

    inline flatness estimation

--- libulc/ulcEncoder_Psycho.h
+++ libulc/ulcEncoder_Psycho.h
@@ -9,74 +9,69 @@
 #include <stddef.h>
 #include <stdint.h>
 /**************************************/
+#include "ulcEncoder.h"
 #include "ulcEncoder_Helper.h"
 /**************************************/
 
-//! Number of spectral flatness bands
-#define FLATNESS_COUNT 32
-
-/**************************************/
-
-//! Compute flatness bands
-static void Block_Transform_ComputeFlatness(const float *Coef, float *Flatness, size_t BlockSize) {
-	size_t i, Width = BlockSize / FLATNESS_COUNT;
-	for(i=0;i<BlockSize;i+=Width) *Flatness++ = SpectralFlatness(Coef + i, Width);
-	*Flatness = Flatness[-1]; //! Interpolation
-}
+//! How many masking bands are used per actual band
+#define MASKING_BAND_DECIMATION_FACTOR 8
 
 /**************************************/
 
 //! Compute masking energy estimation
-//! NOTE: Masking is only computed for every 2 coefficients to save processing time
-static inline __attribute__((always_inline)) float Block_Transform_ComputeMaskingPower_Convolve(
+static inline float Block_Transform_ComputeMaskingPower(
+	const float *Coef,
+	const float *CoefNp,
 	size_t Band,
-	size_t BlockSize,
-	const float *Coef,
-	float NyquistHz,
-	float Flat
-) {
-	//! These settings are mostly based on trial and error
-	//! Flatter bands have a wider masking bandwidth than diffuse ones
-	float Bw = Flat*MaskingBandwidth((Band+1.0f) * NyquistHz/BlockSize) * BlockSize/NyquistHz;
-	float BandPow = sqrtf(SQR(Coef[Band]) + SQR(Coef[Band+1])) * (float)M_SQRT1_2;
-	float MaskFW  = 1.0f - BandPow;
-	float MaskBW  = sqrtf(1.0f - MaskFW);
-	size_t BwFW   = (size_t)(Bw * MaskFW); if(Band+2+BwFW > BlockSize) BwFW = BlockSize - (Band+2);
-	size_t BwBW   = (size_t)(Bw * MaskBW); if(Band        < BwBW)      BwBW = Band;
-	if(!BwFW && !BwBW) return 0.0f;
-
-	//! Perform convolution
-	//! Scaling by 1+Flatness seems to give better results
-	float Sum = 0.0f, Scale = (1.0f+Flat) / (BwFW + BwBW);
-	if(BwFW) do Sum += SQR(Coef[Band+BwFW+1]); while(--BwFW);
-	if(BwBW) do Sum += SQR(Coef[Band-BwBW  ]); while(--BwBW);
-	return Sum*Scale;
-}
-static void Block_Transform_ComputeMaskingPower(
-	const float *Coef,
-	float *MaskingPower,
-	const float *Flatness,
 	size_t BlockSize,
 	float NyquistHz
 ) {
-	size_t i;
-	float Flat_mu   = 0.0f;
-	float Flat_Step = (2.0f*FLATNESS_COUNT) / BlockSize;
-	float Flat_Cur  = *Flatness++;
-	float Flat_Nxt  = *Flatness++;
-	for(i=0;i<BlockSize;i+=2) {
-		//! Get flatness and step through
-		float Flat = Flat_Cur*(1.0f - Flat_mu) + Flat_Nxt*Flat_mu;
-		Flat_mu += Flat_Step;
-		if(Flat_mu >= 1.0f) {
-			Flat_mu -= 1.0f;
-			Flat_Cur = Flat_Nxt;
-			Flat_Nxt = *Flatness++;
-		}
+#if ULC_USE_PSYHOACOUSTICS
+	size_t n;
 
-		//! Convolve masking power
-		MaskingPower[i/2] = Block_Transform_ComputeMaskingPower_Convolve(i, BlockSize, Coef, NyquistHz, Flat);
+	//! These settings are mostly based on trial and error
+	float Bw = MaskingBandwidth((Band+MASKING_BAND_DECIMATION_FACTOR*0.5f) * NyquistHz/BlockSize) * BlockSize/NyquistHz;
+	float BandPow  = 0.0f; for(n=0;n<MASKING_BAND_DECIMATION_FACTOR;n++) BandPow += ABS(Coef[Band+n]);
+	      BandPow  = SQR(BandPow / MASKING_BAND_DECIMATION_FACTOR);
+	float MaskFW   = 1.0f - BandPow;
+	float MaskBW   = sqrtf(1.0f - MaskFW);
+	size_t BwFW    = (size_t)(Bw * MaskFW);
+	size_t BwBW    = (size_t)(Bw * MaskBW);
+	if(Band+MASKING_BAND_DECIMATION_FACTOR+BwFW > BlockSize) BwFW = BlockSize - (Band+MASKING_BAND_DECIMATION_FACTOR);
+	if(Band                                     < BwBW)      BwBW = Band;
+	if(!BwFW && !BwBW) return 0.0f;
+
+	//! Perform convolution
+	//! Scaling by a flatness curve seems to give better results,
+	//! where this flatness curve doubles the masking power at
+	//! "medium" flatness (corresponding to complex signals that
+	//! can have MDCT coefficients discarded without too much
+	//! loss of fidelity to the original signal after IMDCT), but
+	//! also drops the masking power to 0 when very tonal
+	//! NOTE: Flatness calculation is inlined to speed things up
+	float SumPow  = 0.0f, Scale = 1.0f / (BwFW + BwBW);
+	float SumFlat = BandPow * CoefNp[Band];
+	for(n=0;n<BwFW;n++) {
+		float Val2  = SQR(Coef  [Band+n+MASKING_BAND_DECIMATION_FACTOR]); SumPow += Val2;
+		float ValNp =    (CoefNp[Band+n+MASKING_BAND_DECIMATION_FACTOR]);
+		SumFlat += Val2 * ValNp;
 	}
+	for(n=0;n<BwBW;n++) {
+		float Val2  = SQR(Coef  [Band-n-1]); SumPow += Val2;
+		float ValNp =    (CoefNp[Band-n-1]);
+		SumFlat += Val2 * ValNp;
+	}
+	float Nrm  = BandPow + SumPow;
+	float Flat = exp2f((Nrm*logf(Nrm) - 2.0f*SumFlat) / (Nrm * logf(BwBW+1+BwFW))) - 1.0f;
+	return SumPow*Scale * exp2f(1.0f - SQR(5.0f*(Flat - 0.6f)));
+#else
+	(void)Coef;
+	(void)CoefNp;
+	(void)Band;
+	(void)BlockSize;
+	(void)NyquistHz;
+	return 0.0f;
+#endif
 }
 
 /**************************************/
