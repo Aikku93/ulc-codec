82996ed117b81942639c942bf5c6ce80577fc80b
Aikku93 <aik@aol.com.au>
2020-03-21 20:59:51 +1100

    size_t -> int, double -> float, better CoefBitRate approximation

--- libulc/ulcEncoder.c
+++ libulc/ulcEncoder.c
@@ -4,7 +4,6 @@
 //! Refer to the project README file for license terms.
 /**************************************/
 #include <math.h>
-#include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 /**************************************/
@@ -37,47 +36,49 @@ int ULC_EncoderState_Init(struct ULC_EncoderState_t *State) {
 	State->BufferData = NULL;
 
 	//! Verify parameters
-	size_t nChan     = State->nChan;
-	size_t BlockSize = State->BlockSize;
+	int nChan     = State->nChan;
+	int BlockSize = State->BlockSize;
 	if(nChan     < MIN_CHANS || nChan     > MAX_CHANS) return -1;
 	if(BlockSize < MIN_BANDS || BlockSize > MAX_BANDS) return -1;
+	if((BlockSize & (-BlockSize)) != BlockSize)        return -1;
+	if(State->BlockOverlap > BlockSize)                return -1;
 	if(State->BlockOverlap % OVERLAP_MULTIPLES)        return -1;
 
 	//! Get buffer offsets+sizes
 	//! PONDER: This... is probably not ideal
-	size_t TransformBuffer_Size    = sizeof(float)                * (nChan* BlockSize   );
-	size_t TransformNepers_Size    = sizeof(float)                * (nChan* BlockSize   );
-	size_t TransformFwdLap_Size    = sizeof(float)                * (nChan*(BlockSize/2));
-	size_t TransformTemp_Size      = sizeof(float)                * (    1* BlockSize   );
-	size_t AnalysisKeys_Size       = sizeof(struct AnalysisKey_t) * (nChan* BlockSize   );
-	size_t QuantsSum_Size          = sizeof(float)                * (nChan* ULC_MAX_QBANDS);
-	size_t QuantsWeight_Size       = sizeof(float)                * (nChan* ULC_MAX_QBANDS);
-	size_t Quants_Size             = sizeof(float)                * (nChan* ULC_MAX_QBANDS);
-	size_t QuantsBw_Size           = sizeof(uint16_t)             * (nChan* ULC_MAX_QBANDS);
-	size_t _TransformBuffer_Size   = sizeof(float*)               * (nChan              );
-	size_t _TransformNepers_Size   = sizeof(float*)               * (nChan              );
-	size_t _TransformFwdLap_Size   = sizeof(float*)               * (nChan              );
-	size_t _QuantsSum_Size         = sizeof(float*)               * (nChan              );
-	size_t _QuantsWeight_Size      = sizeof(float*)               * (nChan              );
-	size_t _Quants_Size            = sizeof(float*)               * (nChan              );
-	size_t _QuantsBw_Size          = sizeof(uint16_t*)            * (nChan              );
-	size_t TransformBuffer_Offs    = 0;
-	size_t TransformNepers_Offs    = TransformBuffer_Offs    + TransformBuffer_Size;
-	size_t TransformFwdLap_Offs    = TransformNepers_Offs    + TransformNepers_Size;
-	size_t TransformTemp_Offs      = TransformFwdLap_Offs    + TransformFwdLap_Size;
-	size_t AnalysisKeys_Offs       = TransformTemp_Offs      + TransformTemp_Size;
-	size_t QuantsSum_Offs          = AnalysisKeys_Offs       + AnalysisKeys_Size;
-	size_t QuantsWeight_Offs       = QuantsSum_Offs          + QuantsSum_Size;
-	size_t Quants_Offs             = QuantsWeight_Offs       + QuantsWeight_Size;
-	size_t QuantsBw_Offs           = Quants_Offs             + Quants_Size;
-	size_t _TransformBuffer_Offs   = QuantsBw_Offs           + QuantsBw_Size;
-	size_t _TransformNepers_Offs   = _TransformBuffer_Offs   + _TransformBuffer_Size;
-	size_t _TransformFwdLap_Offs   = _TransformNepers_Offs   + _TransformNepers_Size;
-	size_t _QuantsSum_Offs         = _TransformFwdLap_Offs   + _TransformFwdLap_Size;
-	size_t _QuantsWeight_Offs      = _QuantsSum_Offs         + _QuantsSum_Size;
-	size_t _Quants_Offs            = _QuantsWeight_Offs      + _QuantsWeight_Size;
-	size_t _QuantsBw_Offs          = _Quants_Offs            + _Quants_Size;
-	size_t AllocSize = _QuantsBw_Offs + _QuantsBw_Size;
+	int TransformBuffer_Size    = sizeof(float)                * (nChan* BlockSize   );
+	int TransformNepers_Size    = sizeof(float)                * (nChan* BlockSize   );
+	int TransformFwdLap_Size    = sizeof(float)                * (nChan*(BlockSize/2));
+	int TransformTemp_Size      = sizeof(float)                * (    1* BlockSize   );
+	int AnalysisKeys_Size       = sizeof(struct AnalysisKey_t) * (nChan* BlockSize   );
+	int QuantsSum_Size          = sizeof(float)                * (nChan* ULC_MAX_QBANDS);
+	int QuantsWeight_Size       = sizeof(float)                * (nChan* ULC_MAX_QBANDS);
+	int Quants_Size             = sizeof(float)                * (nChan* ULC_MAX_QBANDS);
+	int QuantsBw_Size           = sizeof(uint16_t)             * (nChan* ULC_MAX_QBANDS);
+	int _TransformBuffer_Size   = sizeof(float*)               * (nChan              );
+	int _TransformNepers_Size   = sizeof(float*)               * (nChan              );
+	int _TransformFwdLap_Size   = sizeof(float*)               * (nChan              );
+	int _QuantsSum_Size         = sizeof(float*)               * (nChan              );
+	int _QuantsWeight_Size      = sizeof(float*)               * (nChan              );
+	int _Quants_Size            = sizeof(float*)               * (nChan              );
+	int _QuantsBw_Size          = sizeof(uint16_t*)            * (nChan              );
+	int TransformBuffer_Offs    = 0;
+	int TransformNepers_Offs    = TransformBuffer_Offs    + TransformBuffer_Size;
+	int TransformFwdLap_Offs    = TransformNepers_Offs    + TransformNepers_Size;
+	int TransformTemp_Offs      = TransformFwdLap_Offs    + TransformFwdLap_Size;
+	int AnalysisKeys_Offs       = TransformTemp_Offs      + TransformTemp_Size;
+	int QuantsSum_Offs          = AnalysisKeys_Offs       + AnalysisKeys_Size;
+	int QuantsWeight_Offs       = QuantsSum_Offs          + QuantsSum_Size;
+	int Quants_Offs             = QuantsWeight_Offs       + QuantsWeight_Size;
+	int QuantsBw_Offs           = Quants_Offs             + Quants_Size;
+	int _TransformBuffer_Offs   = QuantsBw_Offs           + QuantsBw_Size;
+	int _TransformNepers_Offs   = _TransformBuffer_Offs   + _TransformBuffer_Size;
+	int _TransformFwdLap_Offs   = _TransformNepers_Offs   + _TransformNepers_Size;
+	int _QuantsSum_Offs         = _TransformFwdLap_Offs   + _TransformFwdLap_Size;
+	int _QuantsWeight_Offs      = _QuantsSum_Offs         + _QuantsSum_Size;
+	int _Quants_Offs            = _QuantsWeight_Offs      + _QuantsWeight_Size;
+	int _QuantsBw_Offs          = _Quants_Offs            + _Quants_Size;
+	int AllocSize = _QuantsBw_Offs + _QuantsBw_Size;
 
 	//! Allocate buffer space
 	char *Buf = State->BufferData = malloc(BUFFER_ALIGNMENT-1 + AllocSize);
@@ -85,11 +86,11 @@ int ULC_EncoderState_Init(struct ULC_EncoderState_t *State) {
 
 	//! Set initial state
 	State->BitBudget   = 0.0;
-	State->CoefBitRate = 1.0 / 8.5; //! Initial approximation (reciprocal of bits per nZ coefficient)
+	State->CoefBitRate = -1.0f; //! Will be set on first run
 
 	//! Initialize pointers
-	size_t i, Chan;
-	Buf += -(uintptr_t)Buf % BUFFER_ALIGNMENT;
+	int i, Chan;
+	Buf += (-(uintptr_t)Buf) & (BUFFER_ALIGNMENT-1);
 	State->TransformBuffer   = (float   **)(Buf + _TransformBuffer_Offs);
 	State->TransformNepers   = (float   **)(Buf + _TransformNepers_Offs);
 	State->TransformFwdLap   = (float   **)(Buf + _TransformFwdLap_Offs);
@@ -127,24 +128,32 @@ void ULC_EncoderState_Destroy(struct ULC_EncoderState_t *State) {
 /**************************************/
 
 //! Encode block
-size_t ULC_EncodeBlock(struct ULC_EncoderState_t *State, uint8_t *DstBuffer, const float *SrcData, double RateKbps, float PowerDecay) {
+int ULC_EncodeBlock(struct ULC_EncoderState_t *State, uint8_t *DstBuffer, const float *SrcData, float RateKbps, float PowerDecay) {
 	//! Refill bit budget
-	double AvgBitBudget = RateKbps*1000.0/State->RateHz * State->BlockSize;
+	float AvgBitBudget = RateKbps*1000.0f/State->RateHz * State->BlockSize;
 	State->BitBudget += AvgBitBudget;
 
 	//! Transform input, build keys, and get maximum number of non-zero bands
-	size_t nKeys = Block_Transform(State, SrcData, PowerDecay);
-	size_t nNzMax; {
+	int nKeys = Block_Transform(State, SrcData, RateKbps, PowerDecay);
+	int nNzMax; {
+		//! First run?
+		if(State->CoefBitRate < 0.0f) {
+			//! Experimentally-derived average bitrate
+			float MaxRate = MaxCodingKbps(State->BlockSize, State->nChan, State->RateHz);
+			float CoefRate = (State->RateHz / 1000.0f / SQR(MaxRate)) * (MaxRate + RateKbps);
+			State->CoefBitRate = CoefRate < 1.0f ? CoefRate : 1.0f;
+		}
+
 		//! Limit to 1.25x target bitrate to avoid quality spikes,
 		//! as these have a tendency to sound bad in context
-		double MinBudget = AvgBitBudget*0.75;
-		double MaxBudget = AvgBitBudget*1.25;
+		float MinBudget = AvgBitBudget*0.75f;
+		float MaxBudget = AvgBitBudget*1.25f;
 
 		//! Clip budget to limit, derive nNzMax from that
-		double Budget = State->BitBudget;
+		float Budget = State->BitBudget;
 		if(Budget < MinBudget) Budget = MinBudget;
 		if(Budget > MaxBudget) Budget = MaxBudget;
-		nNzMax = lrint(Budget * State->CoefBitRate);
+		nNzMax = lrintf(Budget * State->CoefBitRate);
 
 		//! Sometimes we may get less non-zero bands than we're
 		//! able to code (eg. during silence), so just clip here
@@ -152,8 +161,8 @@ size_t ULC_EncodeBlock(struct ULC_EncoderState_t *State, uint8_t *DstBuffer, con
 	}
 
 	//! Encode block
-	size_t nNzCoded;
-	size_t BlockBits = Block_Encode(State, DstBuffer, nNzMax, nKeys, &nNzCoded);
+	int nNzCoded;
+	int BlockBits = Block_Encode(State, DstBuffer, nNzMax, nKeys, &nNzCoded);
 
 	//! Update state
 	State->BitBudget -= BlockBits;
@@ -177,11 +186,11 @@ size_t ULC_EncodeBlock(struct ULC_EncoderState_t *State, uint8_t *DstBuffer, con
 		//!   meeting the target; this allows some adaptation to
 		//!   take place, hopefully giving us a more precise cost
 		//!   measurement in future blocks
-		double RateDecayFactor = 0.25 + nNzCoded*0.75/nNzMax;
-		if(RateDecayFactor > 1.0) RateDecayFactor = 1.0;
+		float RateDecayFactor = 0.25f + nNzCoded*0.75f/nNzMax;
+		if(RateDecayFactor > 1.0f) RateDecayFactor = 1.0f;
 
-		double nZCoefCost = (double)nNzCoded / BlockBits;
-		State->CoefBitRate = State->CoefBitRate*(1.0-RateDecayFactor) + nZCoefCost*RateDecayFactor;
+		float nZCoefCost = (float)nNzCoded / BlockBits;
+		State->CoefBitRate = State->CoefBitRate*(1.0f-RateDecayFactor) + nZCoefCost*RateDecayFactor;
 	}
 
 	//! Return block size
