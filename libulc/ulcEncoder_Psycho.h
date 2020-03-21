368f3eee72d2985fc492f823fd0b2a2319a52f7c
Aikku93 <aik@aol.com.au>
2020-03-21 23:16:16 +1100

    float->double for intermediates, clip on masked bandwidth
    There were some numerical instability issues with floats in these calculations; using double-precision fixed them. Clipping on the masked bandwidth also makes more sense and additionally avoids a possible division by 0 when MaskBw==1 (log(1)=0)

--- libulc/ulcEncoder_Psycho.h
+++ libulc/ulcEncoder_Psycho.h
@@ -15,8 +15,8 @@
 //! Masking threshold estimation
 //! NOTE: ERB and flatness calculations are inlined as an optimization
 struct Block_Transform_MaskingState_t {
-	float SumLin;
-	float SumLog;
+	double SumLin;
+	double SumLog;
 	float BwBase;
 	int   BandBeg;
 	int   BandEnd;
@@ -28,8 +28,8 @@ static inline void Block_Transform_MaskingState_Init(
 	int   BlockSize,
 	float NyquistHz
 ) {
-	State->SumLin = SQR(Coef[0]);
-	State->SumLog = SQR(Coef[0]) * CoefNp[0];
+	State->SumLin = SQR((double)Coef[0]);
+	State->SumLog = SQR((double)Coef[0]) * CoefNp[0];
 	State->BwBase = 24.7f*BlockSize/NyquistHz + 0.0539695f;
 	State->BandBeg = 0;
 	State->BandEnd = 0;
@@ -46,18 +46,18 @@ static inline float Block_Transform_UpdateMaskingThreshold(
 	float Bw = State->BwBase + 0.107939f*Band;
 	float MaskFW   = 1.0f - ABS(Coef[Band]);
 	float MaskBW   = sqrtf(1.0f - MaskFW);
-	int   BandEnd  = Band + (int)(Bw * MaskFW + 0.5f);
-	int   BandBeg  = Band - (int)(Bw * MaskBW + 0.5f);
+	int   BandEnd  = Band + (int)ceilf(Bw * MaskFW);
+	int   BandBeg  = Band - (int)ceilf(Bw * MaskBW);
 	if(BandEnd >= BlockSize) BandEnd = BlockSize-1;
 	if(BandBeg <          0) BandBeg = 0;
 
 	//! Update masking calculations
 	{
-#define ADD_TO_STATE(Val, ValNp) State->SumLin += SQR(Val), State->SumLog += SQR(Val)*(ValNp)
-#define SUB_TO_STATE(Val, ValNp) State->SumLin -= SQR(Val), State->SumLog -= SQR(Val)*(ValNp)
+#define ADD_TO_STATE(Val, ValNp) State->SumLin += SQR((double)Val), State->SumLog += SQR((double)Val)*(ValNp)
+#define SUB_TO_STATE(Val, ValNp) State->SumLin -= SQR((double)Val), State->SumLog -= SQR((double)Val)*(ValNp)
 		int Beg = State->BandBeg, End = State->BandEnd;
-		while(BandBeg < Beg) Beg--, ADD_TO_STATE(Coef[Beg], CoefNp[Beg]); //! Expand
-		while(BandEnd > End) End++, ADD_TO_STATE(Coef[End], CoefNp[End]);
+		while(BandBeg < Beg) --Beg, ADD_TO_STATE(Coef[Beg], CoefNp[Beg]); //! Expand
+		while(BandEnd > End) ++End, ADD_TO_STATE(Coef[End], CoefNp[End]);
 		while(BandBeg > Beg) SUB_TO_STATE(Coef[Beg], CoefNp[Beg]), Beg++; //! Contract
 		while(BandEnd < End) SUB_TO_STATE(Coef[End], CoefNp[End]), End--;
 		State->BandBeg = BandBeg, State->BandEnd = BandEnd;
@@ -66,18 +66,16 @@ static inline float Block_Transform_UpdateMaskingThreshold(
 	}
 
 	//! Get final masking threshold and flatness from the intermediates
-	//! NOTE: Limit at (0.5*eps)^2; this would correspond to one single
-	//! coefficient in the sum, so anything smaller than that would be
-	//! rounding error from limited precision representation.
 	//! NOTE: Multiply by two in the flatness equation ONLY. This will
 	//! return the correct masking value, while still using the squared
 	//! coefficients necessary for the calculation, as that equation
 	//! relies on kind of 'comparing' the log/linear values so they must
 	//! match. The reason for this is that log(x^2) = 2*log(x)
 	//! NOTE: 0x1.62E430p-1f = Log[2]
-	float  SumLin = State->SumLin; if(SumLin < SQR(0.5f*ULC_COEF_EPS)) { *_Flat = 1.0f; return 0.0f; }
+	int    MaskBw = BandEnd - BandBeg + 1; if(MaskBw < 2) { *_Flat = 1.0f; return 0.0f; }
+	float  SumLin = State->SumLin;
 	float nSumLog = State->SumLog / SumLin;
-	*_Flat        = expf(0x1.62E430p-1f * ((logf(SumLin) - 2.0f*nSumLog) / logf(BandEnd - BandBeg + 1))) - 1.0f;
+	*_Flat        = expf(0x1.62E430p-1f * ((logf(SumLin) - 2.0f*nSumLog) / logf(MaskBw))) - 1.0f;
 	return nSumLog;
 }
 
