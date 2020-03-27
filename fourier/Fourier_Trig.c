498c992ca5c82c99ce09f9b14530917e382e4e40
Aikku93 <aik@aol.com.au>
2020-03-28 03:01:43 +1100

    use lagrange polynomial approximation

--- fourier/Fourier_Trig.c
+++ fourier/Fourier_Trig.c
@@ -13,35 +13,21 @@
 /*!
 
 8th order cosine approximation
-Estimates Cos[x*Pi/2], x=[-1,1]
-RMSE=0.000000040504495 (PSNR=147.9dB)
-------------------------------
+Estimates Cos[x*Pi/2] (with x=[-1,1]) using Legendre polynomials,
+using the polynomial form of:
+ y = c1 + (c2 + (c3 + (c4 + c5*x^2)*x^2)*x^2)*x^2;
 
-a[x_] := Cos[x*Pi/2];
-b[x_] := (x^2 - 1)*((c1 + (c2 + c3*x^2)*x^2)*x^2 - 1);
-Solve[{
-  a[430/1000] == b[430/1000],
-  a[710/1000] == b[710/1000],
-  a[895/1000] == b[895/1000]
-}, {c1, c2, c3}]
-
-This gives:
-  c1 ~= 0.23
-  c2 ~= -0.02
-  c3 ~= 0.00086
-
-Although SinCos is generally called as x/N with small
-x and large N, the same equation is used for BOTH
-sine /and/ cosine, so we must optimize at both ends.
-And since DCT4 gradually closes in on x=0.5, we must
-optimize the whole space anyway.
+NOTE: This has bad behaviour at x={0,-1,+1} but these values are
+unused in all calls to these functions, so this is still good.
 
 !*/
 /**************************************/
 
-#define C1 ( 0x1.DE9D73D230B71p-3)
-#define C2 (-0x1.46EAA650565C1p-6)
-#define C3 ( 0x1.C20AB73E45F6Bp-11)
+#define C1 ( 0x1.FFFFFFp-1f)
+#define C2 (-0x1.3BD3AEp0f)
+#define C3 ( 0x1.03BDD4p-2f)
+#define C4 (-0x1.550D85p-6f)
+#define C5 ( 0x1.C390DDp-11f)
 
 /**************************************/
 #if defined(__AVX__)
@@ -52,33 +38,33 @@ void Fourier_SinCosAVX(__m256 x, __m256 *Sin, __m256 *Cos) {
 	sx = _mm256_mul_ps(sx, sx);
 	cx = _mm256_mul_ps(cx, cx);
 #if defined(__FMA__)
-	s  = _mm256_set1_ps(C3);
-	c  = _mm256_set1_ps(C3);
+	s  = _mm256_set1_ps(C5);
+	c  = _mm256_set1_ps(C5);
+	s  = _mm256_fmadd_ps(s, sx, _mm256_set1_ps(C4));
+	c  = _mm256_fmadd_ps(c, cx, _mm256_set1_ps(C4));
+	s  = _mm256_fmadd_ps(s, sx, _mm256_set1_ps(C3));
+	c  = _mm256_fmadd_ps(c, cx, _mm256_set1_ps(C3));
 	s  = _mm256_fmadd_ps(s, sx, _mm256_set1_ps(C2));
 	c  = _mm256_fmadd_ps(c, cx, _mm256_set1_ps(C2));
 	s  = _mm256_fmadd_ps(s, sx, _mm256_set1_ps(C1));
 	c  = _mm256_fmadd_ps(c, cx, _mm256_set1_ps(C1));
-	s  = _mm256_fmadd_ps(s, sx, _mm256_set1_ps(-1.0f));
-	c  = _mm256_fmadd_ps(c, cx, _mm256_set1_ps(-1.0f));
-	s  = _mm256_fmsub_ps(s, sx, s);
-	c  = _mm256_fmsub_ps(c, cx, c);
 #else
-	s  = _mm256_mul_ps(sx, _mm256_set1_ps(C3));
-	c  = _mm256_mul_ps(cx, _mm256_set1_ps(C3));
+	s  = _mm256_mul_ps(sx, _mm256_set1_ps(C5));
+	c  = _mm256_mul_ps(cx, _mm256_set1_ps(C5));
+	s  = _mm256_add_ps(s,  _mm256_set1_ps(C4));
+	c  = _mm256_add_ps(c,  _mm256_set1_ps(C4));
+	s  = _mm256_mul_ps(s,  sx);
+	c  = _mm256_mul_ps(c,  cx);
+	s  = _mm256_add_ps(s,  _mm256_set1_ps(C3));
+	c  = _mm256_add_ps(c,  _mm256_set1_ps(C3));
+	s  = _mm256_mul_ps(s,  sx);
+	c  = _mm256_mul_ps(c,  cx);
 	s  = _mm256_add_ps(s,  _mm256_set1_ps(C2));
 	c  = _mm256_add_ps(c,  _mm256_set1_ps(C2));
 	s  = _mm256_mul_ps(s,  sx);
 	c  = _mm256_mul_ps(c,  cx);
 	s  = _mm256_add_ps(s,  _mm256_set1_ps(C1));
 	c  = _mm256_add_ps(c,  _mm256_set1_ps(C1));
-	s  = _mm256_mul_ps(s,  sx);
-	c  = _mm256_mul_ps(c,  cx);
-	s  = _mm256_add_ps(s,  _mm256_set1_ps(-1.0f));
-	c  = _mm256_add_ps(c,  _mm256_set1_ps(-1.0f));
-	sx = _mm256_add_ps(sx, _mm256_set1_ps(-1.0f));
-	cx = _mm256_add_ps(cx, _mm256_set1_ps(-1.0f));
-	s  = _mm256_mul_ps(s,  sx);
-	c  = _mm256_mul_ps(c,  cx);
 #endif
 	*Sin = s;
 	*Cos = c;
@@ -93,33 +79,33 @@ void Fourier_SinCosSSE(__m128 x, __m128 *Sin, __m128 *Cos) {
 	sx = _mm_mul_ps(sx, sx);
 	cx = _mm_mul_ps(cx, cx);
 #if defined(__FMA__)
-	s  = _mm_set1_ps(C3);
-	c  = _mm_set1_ps(C3);
+	s  = _mm_set1_ps(C5);
+	c  = _mm_set1_ps(C5);
+	s  = _mm_fmadd_ps(s, sx, _mm_set1_ps(C4));
+	c  = _mm_fmadd_ps(c, cx, _mm_set1_ps(C4));
+	s  = _mm_fmadd_ps(s, sx, _mm_set1_ps(C3));
+	c  = _mm_fmadd_ps(c, cx, _mm_set1_ps(C3));
 	s  = _mm_fmadd_ps(s, sx, _mm_set1_ps(C2));
 	c  = _mm_fmadd_ps(c, cx, _mm_set1_ps(C2));
 	s  = _mm_fmadd_ps(s, sx, _mm_set1_ps(C1));
 	c  = _mm_fmadd_ps(c, cx, _mm_set1_ps(C1));
-	s  = _mm_fmadd_ps(s, sx, _mm_set1_ps(-1.0f));
-	c  = _mm_fmadd_ps(c, cx, _mm_set1_ps(-1.0f));
-	s  = _mm_fmsub_ps(s, sx, s);
-	c  = _mm_fmsub_ps(c, cx, c);
 #else
-	s  = _mm_mul_ps(sx, _mm_set1_ps(C3));
-	c  = _mm_mul_ps(cx, _mm_set1_ps(C3));
+	s  = _mm_mul_ps(sx, _mm_set1_ps(C5));
+	c  = _mm_mul_ps(cx, _mm_set1_ps(C5));
+	s  = _mm_add_ps(s,  _mm_set1_ps(C4));
+	c  = _mm_add_ps(c,  _mm_set1_ps(C4));
+	s  = _mm_mul_ps(s,  sx);
+	c  = _mm_mul_ps(c,  cx);
+	s  = _mm_add_ps(s,  _mm_set1_ps(C3));
+	c  = _mm_add_ps(c,  _mm_set1_ps(C3));
+	s  = _mm_mul_ps(s,  sx);
+	c  = _mm_mul_ps(c,  cx);
 	s  = _mm_add_ps(s,  _mm_set1_ps(C2));
 	c  = _mm_add_ps(c,  _mm_set1_ps(C2));
 	s  = _mm_mul_ps(s,  sx);
 	c  = _mm_mul_ps(c,  cx);
 	s  = _mm_add_ps(s,  _mm_set1_ps(C1));
 	c  = _mm_add_ps(c,  _mm_set1_ps(C1));
-	s  = _mm_mul_ps(s,  sx);
-	c  = _mm_mul_ps(c,  cx);
-	s  = _mm_add_ps(s,  _mm_set1_ps(-1.0f));
-	c  = _mm_add_ps(c,  _mm_set1_ps(-1.0f));
-	sx = _mm_add_ps(sx, _mm_set1_ps(-1.0f));
-	cx = _mm_add_ps(cx, _mm_set1_ps(-1.0f));
-	s  = _mm_mul_ps(s,  sx);
-	c  = _mm_mul_ps(c,  cx);
 #endif
 	*Sin = s;
 	*Cos = c;
@@ -129,8 +115,8 @@ void Fourier_SinCosSSE(__m128 x, __m128 *Sin, __m128 *Cos) {
 
 void Fourier_SinCos(float x, float *Sin, float *Cos) {
 	float sx = x + -1.0f, cx = x; //! Phase-change sine to cosine
-	sx *= sx; *Sin = (sx + -1.0f)*((C1 + (C2 + C3*sx)*sx)*sx + -1.0f);
-	cx *= cx; *Cos = (cx + -1.0f)*((C1 + (C2 + C3*cx)*cx)*cx + -1.0f);
+	sx *= sx; *Sin = C1 + (C2 + (C3 + (C4 + C5*sx)*sx)*sx)*sx;
+	cx *= cx; *Cos = C1 + (C2 + (C3 + (C4 + C5*cx)*cx)*cx)*cx;
 }
 
 /**************************************/
