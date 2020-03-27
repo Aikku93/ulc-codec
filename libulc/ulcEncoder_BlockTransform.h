2e9fe7f9b022a01a0588b825dc26b5bb5c05a266
Aikku93 <aik@aol.com.au>
2020-03-28 03:29:04 +1100

    remove CopySamples(), new masking equations, add transient detection, update quantizer range equation

--- libulc/ulcEncoder_BlockTransform.h
+++ libulc/ulcEncoder_BlockTransform.h
@@ -20,72 +20,6 @@
 #include "ulcEncoder_Psycho.h"
 /**************************************/
 
-//! Copy scaled samples to buffer
-static inline void Block_Transform_CopySamples(float *DataDst, const float *DataSrc, int N, float Scale) {
-	int i;
-#if defined(__AVX__)
-	__m256 mScale = _mm256_set1_ps(Scale);
-	for(i=0;i<N;i+=64) {
-		__m256 v0 = _mm256_load_ps(DataSrc + i+ 0);
-		__m256 v1 = _mm256_load_ps(DataSrc + i+ 8);
-		__m256 v2 = _mm256_load_ps(DataSrc + i+16);
-		__m256 v3 = _mm256_load_ps(DataSrc + i+24);
-		__m256 v4 = _mm256_load_ps(DataSrc + i+32);
-		__m256 v5 = _mm256_load_ps(DataSrc + i+40);
-		__m256 v6 = _mm256_load_ps(DataSrc + i+48);
-		__m256 v7 = _mm256_load_ps(DataSrc + i+56);
-		v0 = _mm256_mul_ps(v0, mScale);
-		v1 = _mm256_mul_ps(v1, mScale);
-		v2 = _mm256_mul_ps(v2, mScale);
-		v3 = _mm256_mul_ps(v3, mScale);
-		v4 = _mm256_mul_ps(v4, mScale);
-		v5 = _mm256_mul_ps(v5, mScale);
-		v6 = _mm256_mul_ps(v6, mScale);
-		v7 = _mm256_mul_ps(v7, mScale);
-		_mm256_store_ps(DataDst + i+ 0, v0);
-		_mm256_store_ps(DataDst + i+ 8, v1);
-		_mm256_store_ps(DataDst + i+16, v2);
-		_mm256_store_ps(DataDst + i+24, v3);
-		_mm256_store_ps(DataDst + i+32, v4);
-		_mm256_store_ps(DataDst + i+40, v5);
-		_mm256_store_ps(DataDst + i+48, v6);
-		_mm256_store_ps(DataDst + i+56, v7);
-	}
-#elif defined(__SSE__)
-	__m128 mScale = _mm_set1_ps(Scale);
-	for(i=0;i<N;i+=16) {
-		__m128 v0 = _mm_load_ps(DataSrc + i+ 0);
-		__m128 v1 = _mm_load_ps(DataSrc + i+ 4);
-		__m128 v2 = _mm_load_ps(DataSrc + i+ 8);
-		__m128 v3 = _mm_load_ps(DataSrc + i+12);
-		__m128 v4 = _mm_load_ps(DataSrc + i+16);
-		__m128 v5 = _mm_load_ps(DataSrc + i+20);
-		__m128 v6 = _mm_load_ps(DataSrc + i+24);
-		__m128 v7 = _mm_load_ps(DataSrc + i+28);
-		v0 = _mm_mul_ps(v0, mScale);
-		v1 = _mm_mul_ps(v1, mScale);
-		v2 = _mm_mul_ps(v2, mScale);
-		v3 = _mm_mul_ps(v3, mScale);
-		v4 = _mm_mul_ps(v4, mScale);
-		v5 = _mm_mul_ps(v5, mScale);
-		v6 = _mm_mul_ps(v6, mScale);
-		v7 = _mm_mul_ps(v7, mScale);
-		_mm_store_ps(DataDst + i+ 0, v0);
-		_mm_store_ps(DataDst + i+ 4, v1);
-		_mm_store_ps(DataDst + i+ 8, v2);
-		_mm_store_ps(DataDst + i+12, v3);
-		_mm_store_ps(DataDst + i+16, v4);
-		_mm_store_ps(DataDst + i+20, v5);
-		_mm_store_ps(DataDst + i+24, v6);
-		_mm_store_ps(DataDst + i+28, v7);
-	}
-#else
-	for(i=0;i<N;i++) DataDst[i] = DataSrc[i] * Scale;
-#endif
-}
-
-/**************************************/
-
 //! Insert keys for block coefficients
 //! Returns updated number of keys in list
 //! NOTE:
@@ -100,7 +34,6 @@ static inline int Block_Transform_InsertKeys_UpdateQuantizers(
 	struct Block_Transform_InsertKeys_UpdateQuantizers_QuantState_t *State,
 	int Band,
 	float vNp,
-	float Flat,
 	float QuantRange
 ) {
 	if(State->nQBands == ULC_MAX_QBANDS-1) return 0;
@@ -125,10 +58,10 @@ static inline int Block_Transform_InsertKeys_UpdateQuantizers(
 
 	//! Check range against threshold
 #if 0
-	float r = ABS((2.0f+Flat)*State->AvgHi/State->nAvgHi - (1.0f+Flat)*State->AvgLo/State->nAvgLo);
+	float r = ABS(4.0f*State->AvgHi/State->nAvgHi - 3.0f*State->AvgLo/State->nAvgLo);
 	if(r > QuantRange) {
 #else //! Math optimization (avoid division)
-	float r = ABS((2.0f+Flat)*State->AvgHi*State->nAvgLo - (1.0f+Flat)*State->AvgLo*State->nAvgHi);
+	float r = ABS(4.0f*State->AvgHi*State->nAvgLo - 3.0f*State->AvgLo*State->nAvgHi);
 	if(r > QuantRange*State->nAvgLo*State->nAvgHi) {
 #endif
 		//! A very sharp transient inside a small bandwidth
@@ -181,15 +114,15 @@ static inline void Block_Transform_InsertKeys(
 		//! Get final channel value, taking masking and flatness into account
 		//! Scaling by a flatness curve seems to give better results,
 		//! as flatter signals mask much more than tonal ones
-		//! TMN ratio: Around -13dB
-		ValNp = (2.0f+Flat)*ValNp - (1.0f+Flat)*(Mask + 1.5f*(Flat - 1.0f));
+		//! TMN ratio: Around -6dB
+		ValNp = 4.0f*ValNp - 3.0f*(Mask + 0.7f*(Flat - 1.0f));
 #endif
 		//! Update quantizer bands
 		//! NOTE: Pass the /perceived/ level here
 #if ULC_USE_PSYCHOACOUSTICS
-		int QuantBw = Block_Transform_InsertKeys_UpdateQuantizers(&QuantState, Band, ValNp, Flat, QuantRange);
+		int QuantBw = Block_Transform_InsertKeys_UpdateQuantizers(&QuantState, Band, ValNp, QuantRange);
 #else
-		int QuantBw = Block_Transform_InsertKeys_UpdateQuantizers(&QuantState, Band, ValNp, 0.0f, QuantRange);
+		int QuantBw = Block_Transform_InsertKeys_UpdateQuantizers(&QuantState, Band, ValNp, QuantRange);
 #endif
 		if(QuantBw != 0) {
 			QuantState.LastQBandOfs = Band;
@@ -216,26 +149,75 @@ static inline void Block_Transform_InsertKeys(
 //!  -Applies MDCT
 //!  -Stores keys for block coefficients
 //! Returns the number of keys stored
-static inline void Block_Transform_ToNepers(float *Dst, float *Src, int BlockSize) {
+static inline void Block_Transform_ScaleAndToNepers(float *Dst, float *Src, int BlockSize) {
 	int Band;
 	for(Band=0;Band<BlockSize;Band++) {
-		float v = ABS(Src[Band]);
-		      v = (v < 0.5f*ULC_COEF_EPS) ? ULC_COEF_NEPER_OUT_OF_RANGE : logf(v);
-		Dst[Band] = v;
+		float v = Src[Band] * 2.0f/BlockSize;
+		Dst[Band] = (ABS(v) < 0.5f*ULC_COEF_EPS) ? ULC_COEF_NEPER_OUT_OF_RANGE : logf(ABS(v));
+		Src[Band] = v;
 	}
 }
-static int Block_Transform(const struct ULC_EncoderState_t *State, const float *Data, float RateKbps, float PowerDecay) {
+static int Block_Transform(struct ULC_EncoderState_t *State, const float *Data, float RateKbps, float PowerDecay) {
 	int nChan     = State->nChan;
 	int BlockSize = State->BlockSize;
 
 	int Chan;
 	int nKeys = 0;
-	float  AnalysisPowerNp = 0.0f; PowerDecay = logf(PowerDecay);
+	float AnalysisPowerNp = 0.0f; PowerDecay = logf(PowerDecay);
+
+	//! Attempt to find transients in order to decide on an overlap amount
+	int OverlapScale; {
+		//! Divide block into 64-sample subblocks and get their RMS
+		int n;
+		float *SubBlockRMS = State->TransformTemp;
+		for(n=0;n<64;n++) SubBlockRMS[n] = 0.0f;
+		for(Chan=0;Chan<nChan;Chan++) {
+			for(n=0;n<BlockSize;n++) SubBlockRMS[n/64] += SQR(Data[Chan*BlockSize+n]);
+		}
+
+		//! Find peak difference between subblocks
+		//! NOTE: PeakDif is scaled by 2.0 because we used squared inputs,
+		//! and biased by Log[64] because it wasn't normalized. The bias
+		//! is unimportant, however, as it cancels out in the subtraction.
+		float PeakDif = 0.0f;
+		float RMSaNp = State->LastTrackedRMSNp;
+		for(n=0;n<BlockSize/64;n++) {
+			const float Silence = 1.0e-30f;
+			float RMS = SubBlockRMS[n]; if(RMS < Silence) RMS = Silence;
+			float RMSbNp = logf(RMS);
+			float Dif = RMSbNp - RMSaNp;
+			if(Dif < 0.0f) Dif *= -0.5f; //! Release transients are less important
+			if(Dif > PeakDif) PeakDif = Dif;
+
+			//! NOTE: Remove part of the linear term from the last block.
+			//! This reduces overdetection during volume envelopes
+			const float mu = 0.3f;
+			RMSaNp = mu*RMSaNp + (1.0f-mu)*RMSbNp;
+		}
+		State->LastTrackedRMSNp = RMSaNp;
+
+		//! Set overlap size from peak difference
+		//! Target values:
+		//!  3dB (~0.3Np) = 50.0ms
+		//!  6dB (~0.7Np) = 25.0ms
+		//!  9dB (~1.0Np) = 12.5ms, etc.
+		//! ie.
+		//!  OverlapMs = 50 * E^(Log[2] - Np*(Log[2] * (20/Log[10])/3))
+		//! Note that because PeakDif is scaled by 2.0, we must scale
+		//! by 0.5 to account for that (0x1.00E...p0 instead of 0x1.00E...p1).
+		float OverlapSec = (50.0f/1000.0f) * expf(0x1.62E430p-1f - PeakDif*0x1.00E102p0f);
+		float OverlapSmp = OverlapSec*State->RateHz;
+		OverlapScale = lrintf(log2f(BlockSize / (1.0e-30f + OverlapSmp)));
+		if(OverlapScale < 0x0) OverlapScale = 0x0;
+		if(OverlapScale > 0xF) OverlapScale = 0xF;
+		while((BlockSize >> OverlapScale) < State->MinOverlap) OverlapScale--;
+	}
+	State->ThisOverlap = OverlapScale;
 
 	//! Neper range before a quantizer change should happen
 	//! 0x1.51CCA1p2  = Log[(2*7)^2]; Dynamic range of quantized coefficients, in Nepers
 	//! 0x1.62E430p-1 = Log[2]
-	float QuantRange = 0x1.51CCA1p2f * (1.0f - 2.0f*RateKbps/MaxCodingKbps(BlockSize, nChan, State->RateHz));
+	float QuantRange = 0x1.51CCA1p2f * (1.0f - 4.0f*RateKbps/MaxCodingKbps(BlockSize, nChan, State->RateHz));
 	if(QuantRange < 0x1.62E430p-1f) QuantRange = 0x1.62E430p-1f; //! Limit at dynamic range of 2 units
 	for(Chan=0;Chan<nChan;Chan++) {
 		//! Get buffer pointers
@@ -244,13 +226,9 @@ static int Block_Transform(const struct ULC_EncoderState_t *State, const float *
 		float *BufferFwdLap    = State->TransformFwdLap[Chan];
 		float *BufferTemp      = State->TransformTemp;
 
-		//! Fetch sample data, pre-scaled for scaled IMDCT(*2.0/BlockSize)
-		//! NOTE: Sample data temporarily stored to BufferNepers
-		Block_Transform_CopySamples(BufferNepers, Data + Chan*BlockSize, BlockSize, 2.0f/BlockSize);
-
 		//! Apply transforms and insert keys
-		Fourier_MDCT(BufferTransform, BufferNepers, BufferFwdLap, BufferTemp, BlockSize, State->BlockOverlap);
-		Block_Transform_ToNepers(BufferNepers, BufferTransform, BlockSize);
+		Fourier_MDCT(BufferTransform, Data + Chan*BlockSize, BufferFwdLap, BufferTemp, BlockSize, BlockSize >> OverlapScale);
+		Block_Transform_ScaleAndToNepers(BufferNepers, BufferTransform, BlockSize);
 		Block_Transform_InsertKeys(
 			State->AnalysisKeys,
 			BufferTransform,
