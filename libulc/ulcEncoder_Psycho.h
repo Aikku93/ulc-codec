6892188d78e6d32fdb4499896d3e75e214a14c28
Aikku93 <aik@aol.com.au>
2020-04-12 14:39:30 +1000

    revert back to nSumLog as return value

--- libulc/ulcEncoder_Psycho.h
+++ libulc/ulcEncoder_Psycho.h
@@ -76,7 +76,7 @@ static inline float Block_Transform_UpdateMaskingThreshold(
 	float  SumLin = State->SumLin;
 	float nSumLog = State->SumLog / SumLin;
 	*_Flat        = expf(0x1.62E430p-1f * ((logf(SumLin) - 2.0f*nSumLog) / logf(MaskBw))) - 1.0f;
-	return 0.5f*logf(SumLin/MaskBw); //! NOTE: nSumLog is unreliable here; quiet bands are ignored when mixed with loud bands
+	return nSumLog;
 }
 
 /**************************************/
