fc63367ece45d8890a4d2f139cbee70aea68b0de
Aikku93 <aik@aol.com.au>
2020-11-27 21:39:43 +1100

    Update ulcDecoder.h

--- include/ulcDecoder.h
+++ include/ulcDecoder.h
@@ -48,18 +48,13 @@ void ULC_DecoderState_Destroy(struct ULC_DecoderState_t *State);
 
 //! Decode block
 //! NOTE:
-//!  -Maximum size (in bits) for each block is:
-//!    (4 + 12*(MAX_QUANTS-1) + 4*BlockSize)*nChan
-//!   So input buffer should generally contain at least
-//!   that much cached.
-//!  -MAX_QUANTS is an internal value from the encoder, no larger than 48
 //!  -Output data will have its channels arranged sequentially;
 //!   For example:
 //!   {
 //!    0,1,2,3...BlockSize-1, //! Chan0
 //!    0,1,2,3...BlockSize-1, //! Chan1
 //!   }
-//! Returns the number of bits read
+//! Returns the number of bits read.
 int ULC_DecodeBlock(struct ULC_DecoderState_t *State, float *DstData, const uint8_t *SrcBuffer);
 
 /**************************************/
