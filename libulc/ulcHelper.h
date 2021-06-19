0804756b5593232783c0d2705e0008c88f767e0c
Aikku93 <aik@aol.com.au>
2021-06-19 17:24:57 +1000

    refactor, remove log approximation

--- libulc/ulcHelper.h
+++ libulc/ulcHelper.h
@@ -5,102 +5,75 @@
 /**************************************/
 #pragma once
 /**************************************/
+#include "ulcEncoder.h"
+/**************************************/
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 #define SQR(x) ((x)*(x))
 /**************************************/
-
-//! Subblock count for a block
-static inline int ULC_Helper_SubBlockCount(int Decimation) {
-	static const int8_t Count[8] = {
-		1, //! 0001: N/1
-		2, //! 001x: N/2,N/2
-		3, //! 010x: N/4,N/4,N/2
-		3, //! 011x: N/2,N/4,N/4
-		4, //! 100x: N/8,N/8,N/4,N/2
-		4, //! 101x: N/4,N/8,N/8,N/2
-		4, //! 110x: N/2,N/8,N/8,N/4
-		4, //! 111x: N/2,N/4,N/8,N/8
-	};
-	return Count[Decimation >> 1];
-}
+#define ULC_FORCED_INLINE static inline __attribute__((always_inline))
+/**************************************/
 
 //! Subblock decimation pattern
-//! Usage:
-//!  PatternBase = &PatternTable[DecimationNybble >> 1];
-//!  PatternNext = PatternBase;
-//!  for(Coef in Coefs) {
-//!   *Buffer = Coef; Buffer += BlockSize >> (*PatternNext++);
-//!   if(Buffer >= BufferEnd) Buffer -= BufferSize-1, PatternNext = PatternBase;
-//!  }
-//! The decimation nybble comes from Block_Transform_GetLogOverlapScale(),
-//! corresponding to the upper/second nybble for decimated blocks.
-static inline const int8_t *ULC_Helper_SubBlockDecimationPattern(int Decimation) {
-	static const int8_t Pattern[8][4] = {
-		{0},          //! 0001: N/1
-		{1, 1},       //! 001x: N/2,N/2
-		{2, 2, 1},    //! 010x: N/4,N/4,N/2
-		{1, 2, 2},    //! 011x: N/2,N/4,N/4
-		{3, 3, 2, 1}, //! 100x: N/8,N/8,N/4,N/2
-		{2, 3, 3, 1}, //! 101x: N/4,N/8,N/8,N/2
-		{1, 3, 3, 2}, //! 110x: N/2,N/8,N/8,N/4
-		{1, 2, 3, 3}, //! 111x: N/2,N/4,N/8,N/8
+//! Each subblock is coded in 4 bits (LSB to MSB):
+//!  Bit0..2: Subblock shift (ie. BlockSize >> Shift)
+//!  Bit3:    Transient flag (ie. apply overlap scaling to that subblock)
+typedef uint_least16_t ULC_SubBlockDecimationPattern_t;
+ULC_FORCED_INLINE ULC_SubBlockDecimationPattern_t ULC_SubBlockDecimationPattern(int WindowCtrl) {
+	static const ULC_SubBlockDecimationPattern_t Pattern[] = {
+		0x0000 | 0x0000, //! 0000: N/1 (Unused)
+		0x0008 | 0x0008, //! 0001: N/1*
+		0x0011 | 0x0008, //! 0010: N/2*,N/2
+		0x0011 | 0x0080, //! 0011: N/2,N/2*
+		0x0122 | 0x0008, //! 0100: N/4*,N/4,N/2
+		0x0122 | 0x0080, //! 0101: N/4,N/4*,N/2
+		0x0221 | 0x0080, //! 0110: N/2,N/4*,N/4
+		0x0221 | 0x0800, //! 0111: N/2,N/4,N/4*
+		0x1233 | 0x0008, //! 1000: N/8*,N/8,N/4,N/2
+		0x1233 | 0x0080, //! 1001: N/8,N/8*,N/4,N/2
+		0x1332 | 0x0080, //! 1010: N/4,N/8*,N/8,N/2
+		0x1332 | 0x0800, //! 1011: N/4,N/8,N/8*,N/2
+		0x2331 | 0x0080, //! 1100: N/2,N/8*,N/8,N/4
+		0x2331 | 0x0800, //! 1101: N/2,N/8,N/8*,N/4
+		0x3321 | 0x0800, //! 1110: N/2,N/4,N/8*,N/8
+		0x3321 | 0x8000, //! 1111: N/2,N/4,N/8,N/8*
 	};
-	return Pattern[Decimation >> 1];
-}
-
-//! Subblock coefficient interleaving pattern
-//! The decimation nybble comes from Block_Transform_GetLogOverlapScale(),
-//! corresponding to the upper/second nybble for decimated blocks.
-#define ULC_HELPER_SUBBLOCK_INTERLEAVE_MODULO 8u
-static inline const int8_t *ULC_Helper_SubBlockInterleavePattern(int Decimation) {
-	static const int8_t Mapping[8][8] = {
-		{0,0,0,0,0,0,0,0}, //! 0001: N/1
-		{0,1,0,1,0,1,0,1}, //! 001x: N/2,N/2
-		{0,1,2,2,0,1,2,2}, //! 010x: N/4,N/4,N/2
-		{0,0,1,2,0,0,1,2}, //! 011x: N/2,N/4,N/4
-		{0,1,2,2,3,3,3,3}, //! 100x: N/8,N/8,N/4,N/2
-		{0,0,1,2,3,3,3,3}, //! 101x: N/4,N/8,N/8,N/2
-		{0,0,0,0,1,2,3,3}, //! 110x: N/2,N/8,N/8,N/4
-		{0,0,0,0,1,1,2,3}, //! 111x: N/2,N/4,N/8,N/8
-	};
-	return Mapping[Decimation >> 1];
-}
-
-//! Transient subblock index from decimation pattern
-//! Due to the way the decimation pattern is built, the
-//! subblock index is conveniently obtained via a
-//! POPCNT (minus one for the unary 'stop' bit).
-static inline int ULC_Helper_TransientSubBlockIndex(int Decimation) {
-	return __builtin_popcount(Decimation) - 1;
+	return Pattern[WindowCtrl >> 4];
 }
 
 /**************************************/
 
-//! Quick and dirty logarithm
-//! Polynomial (with x = 0.0 .. 1.0):
-//!  a*x + b*x^2 + c*x^3
-//!  a = 1
-//!  b = -15 - 3*Log[2] + 12*Log[4]
-//!  c =  14 + 4*Log[2] - 12*Log[4]
-//! This gives an approximation to Log[1+x], with PSNR = 59.3dB.
-//! The exponent part is then trivial, forming:
-//!  Log[2]*Exponent + Log[1+Mantissa]
-//! NOTE: Returns Log[2^-126] for Log[0].
-//! NOTE: Assumes x is normal (not NaN, +/-inf) and non-negative.
-//! NOTE: Biased by 2^-126 to avoid dealing with subnormals.
-static inline __attribute__((always_inline)) float ULC_FastLnApprox(float x) {
-	union {
-		float f;
-		struct {
-			uint32_t m:23;
-			uint32_t e:9; //! Technically, {e:8,s:1}, but s=0 always
-		};
-	} v = {.f = x + 0x1.0p-126f};
-	uint64_t m = 0xE348115793F6000ull - v.m*0x8C58828E7ull;      //! .61
-		 m = ((uint64_t)v.m*v.m >> 14) * (m >> 29);          //! .64
-		 m = ((uint64_t)v.m     << 41) - m;                  //! .64; final value for mantissa part
-		 m = ((int)v.e - 127)*0xB17217F7D1CF78ll + (m >> 8); //! .56 (SIGNED!)
-	return (int64_t)m * 0x1.0p-56f;
+//! Quantize value (mathematically optimal)
+ULC_FORCED_INLINE int ULC_CompandedQuantizeUnsigned(float v) {
+	//! Given x pre-scaled by the quantizer, and x' being companded x:
+	//!  xq = Floor[x'] + (x - Floor[x']^2 >= (Floor[x']+1)^2 - x)
+	//! ie. We round up when (x'+1)^2 has less error; note the signs,
+	//! as Floor[x']+1 will always overshoot, and Floor[x'] can only
+	//! undershoot, so we avoid Abs[] by respecting this observation.
+	//! Letting u be the rounding term and thus xq = Floor[x'] + u,
+	//! and letting vq = Floor[x'],
+	//!  u = (x - vq^2 >= (vq+1)^2 - x)     // Add x
+	//!    = (2x - vq^2 >= (vq+1)^2)        // Expand (vq+1)^2
+	//!    = (2x - vq^2 >= 1 + 2*vq + vq^2) // Add vq^2
+	//!    = (2x >= 1 + 2*vq + 2vq^2)       // Divide by 2
+	//!    = (x >= 0.5 + vq + vq^2)         // Factor vq+vq^2
+	//!    = (x >= 0.5 + vq*(1+vq))
+	int vq = (int)sqrtf(v);
+	return vq + (v >= 0.5f + vq*(1+vq));
+}
+ULC_FORCED_INLINE int ULC_CompandedQuantize(float v) {
+	int vq = ULC_CompandedQuantizeUnsigned(ABS(v));
+	return (v < 0.0f) ? (-vq) : (+vq);
+}
+
+//! Quantize coefficient
+//! NOTE: Not mathematically optimal, but appears to sound better.
+ULC_FORCED_INLINE int ULC_CompandedQuantizeCoefficientUnsigned(float v) {
+	int vq = (int)(0x1.6A09E6p-1f + sqrtf(v)); //! Sqrt[1/2]
+	return vq + (vq == 0); //! Avoid collapse for coefficients the psy model says we need
+}
+ULC_FORCED_INLINE int ULC_CompandedQuantizeCoefficient(float v) {
+	int vq = ULC_CompandedQuantizeCoefficientUnsigned(ABS(v));
+	return (v < 0.0f) ? (-vq) : (+vq);
 }
 
 /**************************************/
