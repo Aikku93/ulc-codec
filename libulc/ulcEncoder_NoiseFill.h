1bc61b0e3e2dbf423ccab9ea683c70518bb8b949
Aikku93 <aik@aol.com.au>
2021-10-02 20:04:38 +1000

    revert weird hack, correct quantizer scaling

--- libulc/ulcEncoder_NoiseFill.h
+++ libulc/ulcEncoder_NoiseFill.h
@@ -24,13 +24,13 @@ static inline void Block_Transform_CalculateNoiseLogSpectrum(float *Data, void *
 
 	//! Normalize the logarithmic energy and convert to fixed-point
 	Norm = (Norm > 0x1.0p-96f) ? (0x1.FFFFFCp31f / Norm) : 0x1.FFFFFCp127f;
-	float LogScale = 0x1.EC709Cp30f / (N+1); //! (2^32/Log[2^32]) / ((N+1) * (1-29/32)) = (2^32/Log[2^32] / (1-29/32)) / (N+1)
+	float LogScale = 0x1.EC709Cp30f / N; //! (2^32/Log[2^32]) / (N * (1-29/32)) = (2^32/Log[2^32] / (1-29/32)) / N
 	uint32_t *LogPower = (uint32_t*)Temp;
 	for(n=0;n<N;n++) {
 		v = Data[n] * Norm;
 		LogPower[n] = (v <= 1.0f) ? 0 : (uint32_t)(logf(v) * LogScale);
 	}
-	float LogNorm     = 0x1.62E42Fp-1f - logf(Norm); //! Pre-scale by Log[2] to account for noise mean at 0.5
+	float LogNorm     = 0x1.62E430p-1f - logf(Norm); //! Pre-scale by Log[2] to account for noise mean at 0.5
 	float InvLogScale = N * 0x1.0A2B24p-31f;
 
 	//! Thoroughly smooth/flatten out the spectrum for noise analysis.
@@ -63,8 +63,8 @@ static inline void Block_Transform_CalculateNoiseLogSpectrum(float *Data, void *
 			BandLen = New - BandLen;
 		}
 
-		//! Store the geometric mean, with double weight on the center coefficient
-		Data[n] = (int32_t)((Sum + LogPower[n]) / (BandLen+1))*InvLogScale + LogNorm;
+		//! Store the geometric mean
+		Data[n] = (Sum/BandLen)*InvLogScale + LogNorm;
 	}
 }
 
@@ -81,7 +81,7 @@ static int Block_Encode_EncodePass_GetNoiseQ(const float *LogCoef, int Band, int
 	}
 
 	//! Quantize the noise amplitude into final code
-	int NoiseQ = ULC_CompandedQuantizeUnsigned(Amplitude*q);
+	int NoiseQ = ULC_CompandedQuantizeUnsigned(Amplitude*q*2.0f);
 	if(NoiseQ > 0x7) NoiseQ = 0x7;
 	return NoiseQ;
 }
@@ -131,7 +131,7 @@ static void Block_Encode_EncodePass_GetHFExtParams(const float *LogCoef, int Ban
 	}
 
 	//! Quantize amplitude and decay
-	int NoiseQ     = ULC_CompandedQuantizeUnsigned(Amplitude*q * 4.0f); //! <- Account for dynamic range of NoiseQ vs. coefficients
+	int NoiseQ     = ULC_CompandedQuantizeUnsigned(Amplitude*q*8.0f); //! <- Account for dynamic range of NoiseQ vs. coefficients
 	int NoiseDecay = ULC_CompandedQuantizeUnsigned((Decay-1.0f) * -0x1.0p16f); //! (1-Decay) * 2^16
 	if(NoiseQ     > 1 + 0xF) NoiseQ     = 1 + 0xF;
 	if(NoiseDecay <    0x00) NoiseDecay =    0x00;
