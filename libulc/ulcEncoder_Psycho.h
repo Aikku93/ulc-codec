e41449a7826fd6c3f2d51efb75c9b4a835788913
Aikku93 <aik@aol.com.au>
2021-12-11 19:04:25 +1100

    rework and improve

--- libulc/ulcEncoder_Psycho.h
+++ libulc/ulcEncoder_Psycho.h
@@ -12,13 +12,11 @@
 #include "ulcHelper.h"
 /**************************************/
 
-static inline void Block_Transform_CalculatePsychoacoustics(float *MaskingNp, const float *BufferAmp2, uint32_t *BufferTemp, int BlockSize, uint32_t WindowCtrl) {
+static inline void Block_Transform_CalculatePsychoacoustics(float *MaskingNp, float *BufferAmp2, void *BufferTemp, int BlockSize, uint32_t WindowCtrl) {
 	int n;
 	float v;
 
 	//! Compute masking levels for each [sub-]block
-	uint32_t *Energy   = (uint32_t*)(BufferTemp);
-	uint32_t *EnergyNp = (uint32_t*)(BufferTemp + BlockSize); //! NOTE: Aliasing of BufferAmp2[]
 	ULC_SubBlockDecimationPattern_t DecimationPattern = ULC_SubBlockDecimationPattern(WindowCtrl);
 	do {
 		int SubBlockSize = BlockSize >> (DecimationPattern&0x7);
@@ -27,55 +25,59 @@ static inline void Block_Transform_CalculatePsychoacoustics(float *MaskingNp, co
 		float Norm = 0.0f;
 		for(n=0;n<SubBlockSize;n++) if((v = BufferAmp2[n]) > Norm) Norm = v;
 		if(Norm != 0.0f) {
+			int Log2SubBlockSize = 31 - __builtin_clz(SubBlockSize);
+
 			//! Normalize the energy and convert to fixed-point
 			//! This normalization step forces the sums to be as precise as
 			//! possible without overflowing.
-			//! NOTE: The normalization is based on the widest bandwidth we
-			//! will encounter in the loop; the tone analysis is bound by
-			//! 1-LoScale/HiScale (see below for derivation), and the noise
-			//! analysis is bound by (HiScale-LowScale), so we use whichever
-			//! of these is wider.
+			//! NOTE: The normalization of the log values is based on the
+			//! final masking equation, where two values are interpolated:
+			//!  (NoiseMask*(N-n) + ToneMask*n)/N
+			//! 1/N is then factored out, leaving us with this sum:
+			//!  NoiseMask*(N-n) + ToneMask*n
+			//! which has a maxima of Max(Max(NoiseMask), Max(ToneMask))*N.
+			//! NoiseMask and ToneMask are constrained to have a maximum
+			//! value of 2^32-1, meaning that the normalization that will
+			//! avoid overflow but still have maximum precision is 1/N.
 			//! NOTE: Ensure that Energy[] is not zero or division by 0 may
 			//! occur if the accumulated sums are all zeros.
-			//! NOTE: Do NOT remove the square root in computing Energy[].
-			//! Although the difference is marginal, it works better.
 			//! NOTE: If the maximum value will cause overflow to +inf,
 			//! just use the largest possible scaling. This really should
 			//! never happen in practice, but it's here just to be sure.
+			//! NOTE: The output into MaskingNp[] is scaled by 0.5 so that
+			//! we can just subtract this from the log coefficient amplitude
+			//! to get the final level. The reason being that the importance
+			//! level is guided as:
+			//!  Importance = CoefAmplitude + CoefAmplitude-Masking
+			//!             = 2*CoefAmplitude - Masking
+			//! and because we are scale invariant (only need to sort):
+			//!  Importance = CoefAmplitude - 0.5*Masking
+			uint32_t *Energy   = (uint32_t*)BufferTemp;
+			uint32_t *EnergyNp = (uint32_t*)BufferAmp2;
 			Norm = (Norm > 0x1.0p-96f) ? (0x1.FFFFFCp31f / Norm) : 0x1.FFFFFCp127f; //! NOTE: 2^32-eps*2 to ensure we don't overflow
-			float LogScale = 0x1.FBD422p28f / SubBlockSize; //! (2^32/Log[2^32]) / (N * (1-14/22)) = (2^32/Log[2^32] / (1-14/22)) / N (round down)
+			float LogScale = 0x1.715476p27f / SubBlockSize; //! (2^32/Log[2^32]) / N (round down)
 			for(n=0;n<SubBlockSize;n++) {
 				v = BufferAmp2[n] * Norm;
 				EnergyNp[n] = (v <= 1.0f) ? 0 : (uint32_t)(logf(v) * LogScale);
-				v = 0x1.0p16f * sqrtf(v);
 				Energy  [n] = (v <= 1.0f) ? 1 : (uint32_t)v;
 			}
-			float LogNorm     = 0x1.555555p-2f*logf(Norm); //! Log[Norm]/3
-			float InvLogScale = SubBlockSize * -0x1.582318p-31f; //! Inverse, scaled by -1/3 (round up)
+			float LogNorm     = -0.25f*logf(Norm); //! Log[1/Norm] * 1/2 * 1/2 (for converting Power to Amplitude plus weight for coefficient amplitude)
+			float InvLogScale = 0x1.62E430p-30f;   //! Inverse, scaled by (1/2)/SubBlockSize (round up)
 
-			//! Compute expected level of each band's critical bandwidth
-			//! NOTE: We can solve for the maximum bandwidth used in
-			//! practice (given the limited range of the block size) by
-			//! finding the intersection at yMax=SubBlockSize for xMax:
-			//!  yMax = SubBlockSize = xMax*HiRangeScale
-			//!  xMax = SubBlockSize/HiRangeScale
-			//! Then we plug xMax into the bandwidth:
-			//!  yBw = xMax*HiRangeScale - xMax*LoRangeScale
-			//!      = SubBlockSize * (1 - LoRangeScale/HiRangeScale)
-			//! Setting SubBlockSize=1 gives us the normalized bandwidth:
-			//!  MaxBandwidth = 1 - LoRangeScale/HiRangeScale
-			//! NOTE:
-			//!  Input is from Energy[], EnergyNp[]
-			//!  Output is to ToneMaskNp[] (temporary aliasing of MaskingNp[])
-			int ToneBeg = 0, ToneEnd = 0; uint64_t ToneSum  = 0, ToneSumW = 0;
-			uint32_t *ToneMaskNp = (uint32_t*)MaskingNp;
+			//! Extract the tone masking levels (spectral peaks) and
+			//! make the level proportional to the masking bandwidth,
+			//! then extract the noise masking level (geometric mean)
+			//! /without/ making these levels proportional, and then
+			//! add them together for the final output.
+			int ToneBeg  = 0, ToneEnd  = 0; uint64_t ToneSum  = 0, ToneSumW = 0;
+			int NoiseBeg = 0, NoiseEnd = 0; uint32_t NoiseSum = 0;
 			for(n=0;n<SubBlockSize;n++) {
 				//! Re-focus the tone analysis window
-				{
+				uint32_t ToneMask; {
 					int Old, New;
 					const int RangeScaleFxp = 4;
-					const int LoRangeScale = 14; //! Beg = 0.875*Band
-					const int HiRangeScale = 22; //! End = 1.375*Band
+					const int LoRangeScale = 14; //! Beg = (1-0.125)*Band
+					const int HiRangeScale = 22; //! End = (1+0.375)*Band
 
 					//! Remove samples that went out of focus
 					//! NOTE: We skip /at most/ one sample, so don't loop.
@@ -96,71 +98,61 @@ static inline void Block_Transform_CalculatePsychoacoustics(float *MaskingNp, co
 						ToneSumW += Energy[Old];
 						ToneSum  += Energy[Old] * (uint64_t)EnergyNp[Old];
 					} while(++Old < New);
+
+					//! Extract level
+					ToneMask = (ToneSum/ToneSumW);
 				}
 
-				//! Store the expected value for this band.
-				ToneMaskNp[n] = ToneSum / ToneSumW;
-			}
-
-			//! Get the noise masking level for each band (based on the
-			//! output of the tone masking levels) and add this to the
-			//! tone masking level to give the final masking level
-			//! NOTE:
-			//!  Input is from ToneMaskNp[] (temporary aliasing of MaskingNp[])
-			//!  Output is to EnergyNp[]
-			int NoiseBeg = 0, NoiseEnd = 0; uint32_t NoiseSum = 0;
-			int NoiseNormShift = 31 - __builtin_clz(SubBlockSize); //! 1/SubBlockSize. Not sure why it normalizes like this
-			for(n=0;n<SubBlockSize;n++) {
 				//! Re-focus the noise analysis window
-				//! Same idea as above, except only summing the log values
-				//! NOTE: These values were chosen so that they are exactly
-				//! half the bandwidth of the 'main' analysis window. This
-				//! appears to give the best results.
-				//! NOTE: The range will NOT be bound by the rules set out
-				//! earlier, as we re-scale our sum when we hit the end.
-				//! The maximum bandwidth is thus HiScale-LoScale.
-				{
+				uint32_t NoiseMask; {
 					int Old, New;
+					uint32_t MaskBw;
 					const int RangeScaleFxp = 4;
-					const int LoRangeScale = 15; //! Beg = 0.9375*Band
-					const int HiRangeScale = 19; //! End = 1.1875*Band
+					const int LoRangeScale = 15; //! Beg = (1-0.0625)*Band
+					const int HiRangeScale = 21; //! End = (1+0.3125)*Band
 
 					//! Remove samples that went out of focus
 					Old = NoiseBeg >> RangeScaleFxp, NoiseBeg += LoRangeScale;
 					New = NoiseBeg >> RangeScaleFxp;
 					if(Old < New) {
-						NoiseSum -= ToneMaskNp[Old];
+						NoiseSum -= EnergyNp[Old];
 					}
+					MaskBw = New;
 
 					//! Add samples that came into focus
-					int NewBeg = New;
+					//! NOTE: Clip New /before/ updating MaskBw or the
+					//! geometric mean will turn out wrong by addition
+					//! of implicit 0 padding
 					Old = NoiseEnd >> RangeScaleFxp, NoiseEnd += HiRangeScale;
-					New = NoiseEnd >> RangeScaleFxp;
-					if(New > SubBlockSize) {
-						//! When we've gone out of range, re-scale the sum
-						//! NOTE: (NewEnd-NewBeg) / (OldEnd-NewBeg).
-						//! I haven't gone crazy. We use NewBeg in the divisor
-						//! because we've already removed samples from OldBeg,
-						//! so we must use NewBeg as a reference point.
-						NoiseSum = NoiseSum * (uint64_t)(New-NewBeg) / (Old-NewBeg);
-					} else do {
-						NoiseSum += ToneMaskNp[Old];
+					New = NoiseEnd >> RangeScaleFxp; if(New > SubBlockSize) New = SubBlockSize;
+					if(Old < New) do {
+						NoiseSum += EnergyNp[Old];
 					} while(++Old < New);
+					MaskBw = New - MaskBw;
+
+					//! Extract level
+					NoiseMask = NoiseSum/MaskBw;
 				}
 
-				//! Store the noise-corrected value for this band.
-				EnergyNp[n] = ToneMaskNp[n] + (NoiseSum >> NoiseNormShift);
+				//! Final masking level calculation:
+				//!  NoiseMask + (ToneMask-NoiseMask)*n/N
+				//!  =(NoiseMask*N + (ToneMask-NoiseMask)*n) / N
+				//! Then factor out 1/N and combine into InvLogScale.
+				//! The idea is that lower frequencies will rely more
+				//! on the noise masking level (which is computed to
+				//! be close to the noise floor), while frequencies
+				//! that are much higher in the spectrum will rely on
+				//! the pseudo-peak "tone" masking level. This tends
+				//! to stabilize low/mid-freq tones while preserving
+				//! brightness in the higher frequencies.
+				//! NOTE: This equation can be refactored as:
+				//!  (NoiseMask*(N-n) + ToneMask*n)/N
+				//! Meaning that as long as we don't drop bits, the output
+				//! is always UNSIGNED and we don't need to do anything
+				//! special, as long as wraparound is observed in the sum;
+				//! this form just trades a multiply for a shift.
+				MaskingNp[n] = ((NoiseMask<<Log2SubBlockSize) + (ToneMask-NoiseMask)*n)*InvLogScale + LogNorm;
 			}
-
-			//! The overall idea is to implement this equation:
-			//!  ImportanceLevel = CoefAmplitude * CoefPower/CritBandPower
-			//! Since we're working in the log domain, and the values
-			//! are scale-invariant (only used for comparing):
-			//!  LogImportanceLevel = 3*Log[CoefAmplitude] - Log[CritBandPower]
-			//!                     = Log[CoefAmplitude] - Log[CritBandPower]/3
-			//! NOTE: This step cannot be combined with the
-			//! loop from above, as the pointers are aliased.
-			for(n=0;n<SubBlockSize;n++) MaskingNp[n] = (float)EnergyNp[n]*InvLogScale + LogNorm;
 		}
 
 		//! Move to next subblock
